e9a7f4358e261f1307f0a5923ce403a3793a983b
mclim <martin.lim@rsa.rohde-schwarz.com>
2018-05-01 18:17:23 -0700

    Add Set_FSWIPAdd; Set_Input; Get_EM; Get_EVM_Params Referenced functions to self.

--- driver/VSE_K96.py
+++ driver/VSE_K96.py
@@ -2,9 +2,8 @@
 ### Rohde & Schwarz Automation for demonstration use.
 ###
 ### Purpose: Vector Signal Explorer K96 Functions
-### Author:  Martin C Lim
-### Date:    2018.04.27
-### Requird: python -m pip install pyvisa
+### Author : Martin C Lim
+### Date   : 2018.04.27
 import VSE_Common
 
 class VSE(VSE_Common.VSE):
@@ -14,54 +13,68 @@ class VSE(VSE_Common.VSE):
    #####################################################################
    ### VSE General Settings
    #####################################################################
-   def Set_Autolevel(sState):
-   K2.write('CONF:POW:AUTO %s;*WAI'%sState);
-
-   def Set_BurstSearch(sState):
-      K2.write('DEM:FORM:BURS %s;*WAI'%sState);
+   def Set_Init_K96(self):
+      self.Set_Channel('OFDMVSA')
       
-   def Set_ConfigFile(sFile):
-      K2.write('MMEM:LOAD:CFGF "%s";*WAI'%sFile);
+   def Set_Autolevel(self,sState):
+      self.write('CONF:POW:AUTO %s;*WAI'%sState);  #ON|OFF|1|0
+
+   def Set_BurstSearch(self,sState):
+      self.write('DEM:FORM:BURS %s;*WAI'%sState);  #ON|OFF|1|0
+      
+   def Set_ConfigFile(self,sFile):
+      self.write('MMEM:LOAD:CFGF "%s";*WAI'%sFile);
    
-   def Set_FilterAdjustable(sState):
-      K2.write('INP:FILT:CHAN:STAT %s'%sState);	    	
+   def Set_FilterAdjustable(self,sState):
+      self.write('INP:FILT:CHAN:STAT %s'%sState);  #ON|OFF|1|0
+
+   def Set_FSWIPAdd(self,sIP):                     #For FS-K96
+      self.write('CONF:ADDR "TCPIP::%s"'%sIP);     #FSW IP Address
+ 
+   def Set_Input(self,sType):
+      self.write('INP:SEL %s'%sType);              #RF|AIQ|DIQ|FILE
+
+   def Get_EVM(self):
+      EVM = self.query('FETC:SUMM:EVM?;*WAI')
+      return float(EVM.strip())
+
+   def Get_EVM_Params(self):
+      MAttn   = self.Get_AttnMech()
+      RefLvl  = self.Get_RefLevel()
+      Power   = self.Get_ChPwr()
+      EVM     = self.Get_EVM()
+      return ("%.2f,%.2f,%6.2f,%.2f"%(MAttn,RefLvl,Power,EVM))
       
-   def Set_FSWIPAdd(sIP):
-      K2.write('CONF:ADDR "TCPIP::%s"'%sIP);	#FSW IP Address
-
-   def Set_Input(sType):
-      K2.write('INP:SEL %s'%sType);	    	#RF|AIQ|DIQ|FILE
-
    #####################################################################
    ### Helper Functions
    #####################################################################
-   def EVM_Wait():
-      EVM = '';                              #init EVM read value
-      K2.write('INIT:IMM');                  #Start Measurement
-      while (EVM == ''):     		  #Loop until EVM present
+   def EVM_Wait(self):
+      EVM = '';                           #init EVM read value
+      self.write('INIT:IMM');             #Start Measurement
+      while (EVM == ''):                  #Loop until EVM present
          try:
-            EVM = K2.query('FETC:SUMM:EVM?');
+            EVM = self.Get_EVM();
          except:
             pass
-      asdf = K2.query('FETC:SUMM:EVM?;*WAI');            #Flush buffer of NaN
+      asdf = self.Get_EVM();              #Flush buffer of NaN
      
-   def EVM_AutoCal():
+   def EVM_AutoCal(self):
       #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
       #%%%% Code Settings
       #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
       debug=0;
       Backoff = 0;
-      EVM_Wind = -0.1;              #EVM can degrade by this amount.
-                                    #EVM Repeatability indicator
+      EVM_Wind = -0.1;                    #EVM can degrade by this amount.
+                                          #EVM Repeatability indicator
       #autostart = tic;
       #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
       #%%%% Code Start
       #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
       if debug==1: print('   Autolvl EVM: ');
-      Set_Autolevel("ON")
-      EVM_Curr = Get_EVM();         #Set initial RefLvl & Attn
-      RefLvl = Get_RefLevel()
-      MAttn  = Get_AttnMech()
+      self.Set_Autolevel("ON")
+      EVM_Curr = self.Get_EVM();          #Set initial RefLvl & Attn
+      RefLvl = self.Get_RefLevel()
+      MAttn  = self.Get_AttnMech()
       if debug==1: print ("      Ref:%.2f MAttn:%.0f EVM:%.2f"%(RefLvl, MAttn, EVM_Curr))
       
       #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -70,28 +83,28 @@ class VSE(VSE_Common.VSE):
       if debug==1: print('   Attenu Swp: ');
       EVM_Prev = 1.00; 
       i=0;
-      Set_Autolevel("OFF")            #Manually Set RefLvl & Attn
+      self.Set_Autolevel("OFF")            #Manually Set RefLvl & Attn
       while (i <= MAttn) & (i < 30):
          MechAttn = MAttn - i;
-         Set_AttnMech(MAttn - i)
-         EVM_Curr = Get_EVM();
+         self.Set_AttnMech(MAttn - i)
+         EVM_Curr = self.Get_EVM();
          if debug==1: print ("      Ref:%.2f MAttn:%.0f EVM:%.2f"%(RefLvl, MechAttn, EVM_Curr))
          if EVM_Curr =='NAN':
             print "EVM NAN"
             break
 
-         Diff = EVM_Prev - EVM_Curr;   #Positive = improvedEVM
+         Diff = EVM_Prev - EVM_Curr;      #Positive = improvedEVM
          if (Diff > EVM_Wind):
             EVM_Prev = EVM_Curr;
             i = i + 1;
          else:
             if debug==1: print("      Break")
-            i = i - 1;                 #Previous value
+            i = i - 1;                    #Previous value
             break
-      MechAttn = MAttn - i + Backoff;  #MechAttn Used for next step
+      MechAttn = MAttn - i + Backoff;     #MechAttn Used for next step
       if (MechAttn < 0):
          MechAttn= 0
-      Set_AttnMech(MechAttn)
+      self.Set_AttnMech(MechAttn)
          
       #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
       #%%%% Ref Sweep
@@ -99,10 +112,10 @@ class VSE(VSE_Common.VSE):
       #print('   RefLvl Swp: ');
       EVM_Prev = 1.00; 
       i=0;
-      Set_Autolevel("OFF")            #Manually Set RefLvl & Attn
+      Set_Autolevel("OFF")                #Manually Set RefLvl & Attn
       for x in range(0):
          Set_RefLevel(RefLvl - i)
-         EVM_Curr = Get_EVM();
+         EVM_Curr = self.Get_EVM();
          if debug==1: print ("      Ref:%.2f MAttn:%.0f EVM:%.2f"%(RefLvl-i, MechAttn, EVM_Curr))
          if EVM_Curr =='NAN':
             print "EVM NAN"
@@ -114,9 +127,9 @@ class VSE(VSE_Common.VSE):
             i = i + 1;
          else:
             i = i - 1;                 #Previous value
-            Set_RefLevel(RefLvl - i)
+            self.Set_RefLevel(RefLvl - i)
             break
-      EVM_Wait()
+      self.EVM_Wait()
 
 #####################################################################
 ### Run if Main
@@ -128,8 +141,7 @@ if __name__ == "__main__":
       print(sys.version)
    VSE = VSE()
    VSE.VISA_Open("127.0.0.1")
-   VSE.Set_Channel("ADEM")
    VSE.Set_DisplayUpdate('ON')
-   VSE.Set_Adem_dbw(500e6)
+   VSE.Get_EVM_Params()
    VSE.Set_InitImm()
  
