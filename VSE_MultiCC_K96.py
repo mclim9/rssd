56afdf256b3c504fd3be8c4f3673cfdbc65745bd
mclim <martin.lim@rsa.rohde-schwarz.com>
2018-05-01 19:44:21 -0700

    Add user input parameters Add Setup section Add Measurement section

--- VSE_MultiCC_K96.py
+++ VSE_MultiCC_K96.py
@@ -7,26 +7,42 @@
 ##########################################################
 ### User Entry
 ##########################################################
-host = '127.0.0.1'               #Get local machine name
-port = 5025                      #Reserve a port for your service.
-
-import driver.VSE_K96
+VSE_IP = '127.0.0.1'                  #Get local machine name
+FSW_IP = '192.168.1.109'
+Freq = 29e6
+CC_Size = 100e6
 
 ##########################################################
 ### Code Start
 ##########################################################
-VSE = driver.VSE_K96.VSE()
-if 0:
-   VSE.VISA_Open("192.168.1.109")
-else:
-   VSE.VISA_Open("127.0.0.1")
-   
-VSE.Set_DisplayUpdate("ON")
-VSE.Set_Group("Group2")
-VSE.Set_Channel("OFDMVSA")
-VSE.Set_Channel("OFDMVSA","K962")
-VSE.Set_SweepCont(0)
-VSE.Set_InitImm()
+import driver.VSE_K96
+
+##########################################################
+### Setup 
+##########################################################
+VSE = driver.VSE_K96.VSE()          #Create VSE Object
+VSE.VISA_Open(VSE_IP)               #Connect to VSE
+#VSE.Set_FSWIPAdd(FSW_IP)            #Define FSW_IP
+VSE.Set_Init_K96()                  #Change Channel
+VSE.Set_DisplayUpdate("ON")         #Display On
+
+for i in range(0,0):
+   #VSE.Set_Group("Group2")             #Create Group
+   #VSE.Set_Channel("OFDMVSA")          #Create Channel
+   #VSE.Set_Channel("OFDMVSA","K962")   #Create Channel 
+   VSE.Set_Input("File")
+   VSE.Set_InputFile("")
+   VSE.Set_Freq(Freq+i*CC_Size)
+   VSE.Set_ConfigFile("C:\\Users\\LIM_M\\ownCloud\\ATE\\00_Code\\RS_ATE_Python2\\utils\\BBAnalog_1CC_100RB_64QAM_IQ-17symC.xml")
+   VSE.Set_SweepCont(0)                #Single Sweep
+
+##########################################################
+### Measure
+##########################################################
+VSE.Set_InitImm()                   #Take single sweep
+VSE.EVM_Wait()                      #Wait for EVM ready
+EVM_Meas = VSE.Get_EVM_Params()
+print(EVM_Meas)
 VSE.VISA_ClrErr()
  
 
