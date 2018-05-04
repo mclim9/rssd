de7c5801156953413d5e809d864c3bcc7586e12d
mclim <martin.lim@rsa.rohde-schwarz.com>
2018-05-04 11:47:37 -0700

    Chg K2 to self.  First prototype

--- driver/SMW_Common.py
+++ driver/SMW_Common.py
@@ -2,10 +2,10 @@
 ### Rohde & Schwarz Automation for demonstration use.
 ###
 ### Purpose: Vector Signal Generator Common Functions
-### Author:  Martin C Lim
-### Date:    2018.02.01
+### Author : Martin C Lim
+### Date   : 2018.02.01
 ### Requird: python -m pip install pyvisa
-###          libVISA_class
+#####################################################################
 import yaVISA
 
 class VSG(yaVISA.RSVisa):
@@ -16,25 +16,25 @@ class VSG(yaVISA.RSVisa):
    ### SMW Arb
    #####################################################################
    def Set_ArbNextSeg(self,num):
-      K2.query('BB:ARB:WSEG:NEXT %d;*OPC?'%d)
+      self.query('BB:ARB:WSEG:NEXT %d;*OPC?'%d)
 
    def Set_ArbState(self,sState):
-      K2.query('BB:ARB:STATE %s;*OPC?'%sState)
+      self.query('BB:ARB:STATE %s;*OPC?'%sState)
 
    def Set_ArbWv(self,InWv):
-       K2.query('BB:ARB:WAV:SEL "%s"; *OPC?'%InWv)
+       self.query('BB:ARB:WAV:SEL "%s"; *OPC?'%InWv)
       
    def Set_ArbSeg(self,Seg):
-       K2.write('SOUR:BB:ARB:WSEG:NEXT %d'%Seg)
-       K2.write('SOUR:BB:ARB:WSEG:NEXT:EXEC')
+       self.write('SOUR:BB:ARB:WSEG:NEXT %d'%Seg)
+       self.write('SOUR:BB:ARB:WSEG:NEXT:EXEC')
 
    def Get_ArbClockFreq(self):
-      SCPI = K2.query('SOUR:BB:ARB:CLOC?')
+      SCPI = self.query('SOUR:BB:ARB:CLOC?')
       return float(SCPI)
 
    def Get_ArbTime(self):
       Fs = Get_ArbClockFreq()
-      Points = K2.query('BB:ARB:WAV:POIN?').strip()
+      Points = self.query('BB:ARB:WAV:POIN?').strip()
       WvTime = int(Points)/int(Fs)
       return WvTime
       
@@ -42,39 +42,39 @@ class VSG(yaVISA.RSVisa):
    ### SMW Generic
    #####################################################################
    def Init_Wideband(self):
-      K2.write('SOUR:POW:ATT:DIG 3')         #Set Digital Attenuation
-      K2.write('POW:ALC:STATE AUTO')         #Turn ALC ON|OFF|OFFT|AUTO|
-      K2.write('SOUR:POW:ALC:DAMP AUTO')     #Turn Driver AMP ON|OFF|AUTO
-      K2.write('SOUR:BB:IQG DB8')            #Baseband IQ gain
+      self.write('SOUR:POW:ATT:DIG 3')         #Set Digital Attenuation
+      self.write('POW:ALC:STATE AUTO')         #Turn ALC ON|OFF|OFFT|AUTO|
+      self.write('SOUR:POW:ALC:DAMP AUTO')     #Turn Driver AMP ON|OFF|AUTO
+      self.write('SOUR:BB:IQG DB8')            #Baseband IQ gain
       
       ## Not so critical
-      K2.write('SOUR:AWGN:STAT 0')           #Turn AWGN off (default)
-      K2.write('BBIN:STAT OFF')              #Turn BB Input off(default)
+      self.write('SOUR:AWGN:STAT 0')           #Turn AWGN off (default)
+      self.write('BBIN:STAT OFF')              #Turn BB Input off(default)
 
    def Set_Freq(self,freq):
-      K2.write(':SOUR1:FREQ:CW %f'%freq);    #RF Freq
+      self.write(':SOUR1:FREQ:CW %f'%freq);    #RF Freq
 
    def Set_RFPwr(self,fPow):   #fPow
-      K2.write('SOUR:POW %f'%fPow);          #RF Pwr
+      self.write('SOUR:POW %f'%fPow);          #RF Pwr
       
    def Set_RFState(self,sState):
-      K2.query('OUTP %s;*OPC?'%sState)
+      self.query('OUTP %s;*OPC?'%sState)
 
    def Set_DriveAmp(self,sState):
       ### ON, OFF, AUTO, FIX, 
-      K2.query('SOUR:POW:ALC:DAMP %s;*OPC?'%sState)
+      self.query('SOUR:POW:ALC:DAMP %s;*OPC?'%sState)
       
 
    #####################################################################
    ### Verizon 5G
    #####################################################################
    def Set_V5GState(self,sState):
-      K2.write('SOUR1:BB:V5G:STAT %s;*OPC?'%sState)
+      self.write('SOUR1:BB:V5G:STAT %s;*OPC?'%sState)
 
    def Set_V5G_Wave(self,sWaveform):
       ### sWaveform = Uplink_Config_1
-      K2.query('SOUR1:BB:V5G:SETT:PCON "%s";*OPC?'%sWaveform)
-      #K2.write('SOUR1:BB:V5G:SETT:PCON "%s"'%sWaveform)
+      self.query('SOUR1:BB:V5G:SETT:PCON "%s";*OPC?'%sWaveform)
+      #self.write('SOUR1:BB:V5G:SETT:PCON "%s"'%sWaveform)
 
 
 #####################################################################
@@ -84,4 +84,4 @@ if __name__ == "__main__":
    # this won't be run when imported
    SMW = VSG()
    SMW.VISA_Open("192.168.1.114")
-   SMW.Set_RFPwr(-10)
+   SMW.Set_Freq(29e9)
