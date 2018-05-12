852f297a3954095f525d5631a64986d65123b044
mclim <martin.lim@rsa.rohde-schwarz.com>
2018-05-11 21:41:08 -0700

    added tests

--- test/test_FSW_Common.py
+++ test/test_FSW_Common.py
@@ -13,19 +13,34 @@ port = 5025                      #Reserve a port for your service.
 ##########################################################
 ### Code Start
 ##########################################################
-#from RS_ATE_Python2.driver.FSW_Common import VSA
-from driver.FSW_Common import VSA
-#import .driver.FSW_Common
+from rssd.FSW_Common import VSA
 import unittest
 
-class TestGeneral(unittest.Testcase):
+class TestGeneral(unittest.TestCase):
    def setUp(self):                 #run before each test
-      print("Setup")
-
-   def test_FSW_Connect(self):
       FSW = VSA()
       FSW.VISA_Open(host)
-      FSW.Set_DisplayUpdate("ON")
+      FSW.VISA_Reset()
+      FSW.VISA_ClrErr()
+      FSW.dLastErr = ""
+
+   def test_FSW_Connect(self):
+      self.assertEqual(FSW.Make,"Rohde&Schwarz")
+
+   def test_FSW_Common(self):      
+      FSW.Set_Freq(1e6)
+      FSW.Set_RefLevel(10)
+      FSW.Set_ResBW(1e6)
+      FSW.Set_VidBW(1e6)
+      FSW.Set_Span(100e6)
+      FSW.Get_Attn()
+      FSW.Get_RefLevel()
+      self.assertEqual(FSW.dLastErr,"")
+
+   def test_FSW_Marker(self):      
+      FSW.Set_Mkr_Peak()
+      FSW.Get_Mkr_Freq()
+      self.assertEqual(FSW.dLastErr,"")
       
 
 if __name__ == '__main__':
