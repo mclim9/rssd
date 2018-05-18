273bbb0e2417398c5d9f84b25b470c1b3aca6591
Martin Lim <martin.lim@rsa.rohde-schwarz.com>
2018-05-18 16:28:55 -0700

    Appended 5GNR to methods.

--- rssd/FSW_5GNR_K144.py
+++ rssd/FSW_5GNR_K144.py
@@ -9,58 +9,61 @@ import FSW_Common
 
 class VSA(FSW_Common.VSA):
    def __init__(self):
-      pass
-
+      try:
+         super().__init__()
+      except:
+         super(VSA, self).__init__()
+      
    #####################################################################
    ### FSW V5G
    #####################################################################
    def Init_5GNR(self):
       self.Set_Channel('5GNR')
 
-   def Set_V5G_Allocation(self,sFilename):
+   def Set_5GNR_Allocation(self,sFilename):
       # \Instr\user\V5GTF\AllocationFiles\UL
       self.write('MMEM:LOAD:DEM "%s"'%sFilename);
       
-   def Set_V5G_Direction(self,sDirection):
+   def Set_5GNR_Direction(self,sDirection):
       # sDirection = "UL" or "DL"
       self.write(':CONF:V5G:LDIR %s'%sDirection);
 
-   def Set_V5G_AutoEVM(self):
+   def Set_5GNR_AutoEVM(self):
       self.write(':SENS:ADJ:EVM;*WAI');
       #VISA_OPC_Wait(K2, ':SENS:ADJ:EVM;*WAI')
       
    #####################################################################
    ### FSW LTE Settings
    #####################################################################
-   def Set_LTE_Modulation(self,iMod):
+   def Set_5GNR_Modulation(self,iMod):
       self.write(':CONF:LTE:UL:SUBF0:ALL:MOD QPSK')
 
-   def Set_LTE_ResourceBlock(self,iRB):
+   def Set_5GNR_ResourceBlock(self,iRB):
       self.write(':CONF:LTE:UL:SUBF0:ALL:RBC %d'%iRB)
 
-   def Set_LTE_ResourceBlockOffset(self,iRBO):
+   def Set_5GNR_ResourceBlockOffset(self,iRBO):
       self.write(':CONF:LTE:UL:SUBF0:ALL:RBOF %d'%iRBO)
 
    #####################################################################
    ### FSW Common Query
    #####################################################################
-   def Get_ACLR(self):
+   def Get_5GNR_ACLR(self):
       ACLR = self.query(':CALC:MARK:FUNC:POW:RES? MCAC')
       return float(EVM)
 
-   def Get_ChPwr(self):
+   def Get_5GNR_ChPwr(self):
       Power   = float(self.query('FETC:SUMM:POW?'))
       return Power
       
-   def Get_LTE_EVM(self):
+   def Get_5GNR_EVM(self):
       EVM = self.query('FETC:SUMM:EVM?')
       return float(EVM)
 
-   def Get_EVM_n_Params(self):
+   def Get_5GNR_EVMParams(self):
       MAttn   = self.Get_AttnMech()
       RefLvl  = self.Get_RefLevel()
-      Power   = self.Get_ChPwr()
-      EVM     = self.Get_EVM()
+      Power   = self.Get_5GNR_ChPwr()
+      EVM     = self.Get_5GNR_EVM()
       return ("%.2f,%.2f,%6.2f,%.2f"%(MAttn,RefLvl,Power,EVM))
 
 #####################################################################
