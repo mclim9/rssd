12475f39949dd10ad2e994e3862733c23fa73b2d
Martin Lim <martin.lim@rsa.rohde-schwarz.com>
2018-05-17 17:31:52 -0700

    Basic SMW functions

---
+++ rssd/examples/SMW_LoadArb.py
@@ -0,0 +1,25 @@
+##########################################################
+### Rohde & Schwarz Automation for demonstration use.
+###
+### Purpose: Load arb file on SMW
+### Author:  mclim
+### Date:    2018.05.17
+##########################################################
+### User Entry
+##########################################################
+host = '192.168.1.114'               #Get local machine name
+
+##########################################################
+### Code Start
+##########################################################
+from rssd.SMW_Common import VSG
+
+SMW = VSG()
+SMW.VISA_Open(host) 
+#SMW.logSCPI()                    #Log SCPI commands
+SMW.Set_Freq(10e9)               #Set 10GHz
+SMW.Set_RFPwr(-30)               #Output -30dBm
+SMW.Set_RFState('ON')            #Turn RF Output on
+SMW.Set_ArbWv('composer.wv')     #Load file
+SMW.Set_ArbState('ON')           #Turn on Arb & IQ Mod
+SMW.VISA_ClrErr()                #Clear Errors
