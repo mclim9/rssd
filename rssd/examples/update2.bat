12bd0719b24ae19d371f60a5eed8f4af20a84f88
Martin Lim <martin.lim@rsa.rohde-schwarz.com>
2018-05-24 11:28:16 -0700

    slist instead of blist

--- rssd/examples/update2.bat
+++ rssd/examples/update2.bat
@@ -1,3 +1,3 @@
 cd C:\Users\LIM_M\ownCloud\ATE\00_Code\RSSD
-python setup.py bdist
+python setup.py sdist
 python -m pip install .
--- rssd/examples/update3.bat
+++ rssd/examples/update3.bat
@@ -1,3 +1,3 @@
 cd C:\Users\LIM_M\ownCloud\ATE\00_Code\RSSD
-python3 setup.py bdist
+python3 setup.py sdist
 python3 -m pip install .
