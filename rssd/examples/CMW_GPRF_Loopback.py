0bd1a01acf8cd3fbadfd91f3f2241cc62abeaac7
Martin Lim <martin.lim@rsa.rohde-schwarz.com>
2018-06-07 14:24:44 -0700

    Validated on HW

--- rssd/examples/CMW_GPRF_Loopback.py
+++ rssd/examples/CMW_GPRF_Loopback.py
@@ -8,12 +8,27 @@
 ### User Entry
 ##########################################################
 import os
+import tkMessageBox
+
 BaseDir = os.path.dirname(os.path.realpath(__file__))
 OutFile = BaseDir + "\\data\\" + __file__
 InpFile = BaseDir + "\\data\\" + __file__ + ".csv"
 
 visa = '127.0.0.1'               #Get local machine name
-freqArry = [1e9, 2e9, 3e9, 4e9, 5e9]
+repeat = 1
+ports = [1,2,3,4,5,6,7,8]
+
+##########################################################
+### Function Definition
+##########################################################
+def CMW_Set(freq, pwr):
+   CMW.Set_Gen_RFPwr(pwr)
+   CMW.Set_Gen_Freq(freq)
+   CMW.Set_Gen_RFState("ON")
+
+   CMW.Init_MeasPower(i)
+   CMW.Set_VSA_Freq(freq)
+   CMW.Set_VSA_RefLevl(pwr)
 
 ##########################################################
 ### Code Start
@@ -23,34 +38,42 @@ from rssd.CMW_GPRF   import BSE
 from datetime        import datetime
 
 f = FileIO()
+f.debug = 0 
 IArry = f.initread(InpFile).readcsv()
 OFile = f.Init(OutFile)
 
 CMW = BSE()
 CMW.jav_Open("127.0.0.1")
-CMW.Set_Sys_TxPortLoss(1,10)
-CMW.Set_Sys_TxPortLoss(2,30)
-CMW.Set_Sys_RxPortLoss()
+
+for port in ports:
+	CMW.Set_Sys_TxPortLoss(port,0)
+	CMW.Set_Sys_RxPortLoss(port,0)
 CMW.Init_VSG()
 
-for i in range(1,3):
-   for cond in IArry:
-      CMW.Set_Gen_PortON(i)
-      tick = datetime.now()
-      CMW.Set_Gen_RFPwr(cond[0]-50)
-      CMW.Set_Gen_Freq(cond[0])
-      CMW.Set_Gen_RFState("ON")
+OFile.write ('\nDate,Iter,Freq,Pwr,MPwr,Port,Time,Diff')
+for r in range (0,repeat):       #Repeatability Loop
+   print("Loop%d"%r)
+   for cond in IArry:         #Condition Loop
+      for i in [2]:         #Port Loop
+         freq = float(cond[0])
+         pwr  = float(cond[1])
+         CMW.Set_Gen_PortON(i)
+         tick = datetime.now()
+         CMW.Set_Gen_RFPwr(pwr)
+         CMW.Set_Gen_Freq(freq)
+         CMW.Set_Gen_RFState("ON")
 
-      CMW.Init_Power(i)
-      CMW.Set_VSA_Freq(cond[0])
-      CMW.Set_VSA_RefLevl(cond[1])
-      tock = datetime.now()
-      OutStr = '%f,%f,%d,%s'%(freq,CMW.Get_ChPwr()[1],i,tock-tick)
-      OFile.write (OutStr)
-      Set_Gen_PortOFF(i)
-   #end freq loop
-#end port loop   
-
+         CMW.Init_MeasPower(i)
+         CMW.Set_VSA_Freq(freq)
+         CMW.Set_VSA_RefLevl(pwr)
+         tock = datetime.now()
+         MPwr = CMW.Get_ChPwr()[1]
+         OutStr = '%d,%d,%.3f,%.3f,%d,%s,%f'%(r,freq,pwr,MPwr,i,tock-tick,pwr-MPwr)
+         OFile.write (OutStr)
+         #CMW.Set_Gen_PortOFF(i)
+         #exit()
+      #end condition loop
+   #end port loop   
+#end repeatability loop
 CMW.jav_ClrErr()
 CMW.jav_Close()
-OFile.close()
