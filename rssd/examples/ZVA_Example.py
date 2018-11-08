2900180ebb81dce7a0d3659dd75f2142e038b5b1
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2018-11-08 11:59:38 -0800

    Proto

--- rssd/examples/ZVA_Example.py
+++ rssd/examples/ZVA_Example.py
@@ -8,39 +8,27 @@
 ##########################################################
 ### User Entry
 ##########################################################
-SMW_IP   = '192.168.1.114'
-FSW_IP   = '192.168.1.109'
+ZVA_IP   = 'localhost'
 Freq     = 28e9
 ChBW     = 100
 
 ##########################################################
 ### Code Overhead: Import and create objects
 ##########################################################
-from rssd.FSW_Common    import VSA
-from rssd.SMW_Common    import VSG
+from rssd.VNA_Common    import VNA
 from rssd.FileIO        import FileIO
 
 OFile = FileIO().makeFile(__file__)
-SMW = VSG().jav_Open(SMW_IP,OFile)  #Create SMW Object
-#FSW = VSA().jav_Open(FSW_IP,OFile)  #Create FSW Object
+ZVA = VNA().jav_openvisa('TCPIP0::'+ZVA_IP+'::5025::SOCKET',OFile)  #Create SMW Object
 
 ##########################################################
 ### Code Start
 ##########################################################
-# parts
-#scpi = "MMEM:DATA '/var/user/test.txt',#15hallo"
-#scpi = 'BB:ARB:WAV:DATA "test.wv",#14'    #bytes = 2(I)+2(Q)= 4bytes = 1IQ sample
-scpi  = ':MMEM:DATA:UNPR "NVWFM://var//user//wave.wv",#212'
-bits  = b'\x00\x01\x02\x03\x00\x01\x02\x03\x00\x01\x02\x03'
-cmd   = bytes(scpi, 'utf-8') + bits
-print(bytes(cmd))
-SMW.K2.write_raw(cmd)
-SMW.write('SOUR1:BB:ARB:WAV:CLOC "/var/user/wave.wv",1.1E6')
-SMW.write(':MMEM:DATA:UNPR "NVMKR:/var/user/wave.wv",#111')
-SMW.write('BB:ARB:WAV:SEL "/var/user/wave.wv"')
-print(SMW.query('SYST:ERR?'))
+ZVA.Set_FreqStart(2e6)
+ZVA.Set_FreqStop(3e9)
+print(ZVA.query('SYST:ERR?\n\r'))
 
 ##########################################################
 ### Close Nicely
 ##########################################################
-SMW.jav_Close()
+ZVA.jav_Close()
