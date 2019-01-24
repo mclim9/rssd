97e5b1fa223e902ceae242d3874f399dbbfaa5a1
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-01-24 12:36:56 -0800

    add xml output for FSW transceiver.

--- rssd/examples/SMW_FSW_Sweep.py
+++ rssd/examples/SMW_FSW_Sweep.py
@@ -9,8 +9,8 @@
 ##########################################################
 SMW_IP = '192.168.1.115'                    #IP Address
 FSW_IP = '192.168.1.109'                    #IP Address
-FreqStart = int(51e9)
-FreqStop = int(75e9)
+FreqStart = int(24e9)
+FreqStop = int(40e9)
 FreqStep = int(1e9)
 fSpan = 100e6
 SWM_Out = -20
@@ -24,10 +24,10 @@ from rssd.SMW_Common    import VSG
 from rssd.FileIO        import FileIO
 import time
 
-OFile = FileIO().makeFile(__file__)
-SMW = VSG().jav_Open(SMW_IP,OFile)  #Create SMW Object
-FSW = VSA().jav_Open(FSW_IP,OFile)  #Create FSW Object
-
+OFileCSV = FileIO().makeFile('FSMW_SweepCSV')
+OFileXML = FileIO().makeFile('FSMW_SweepXML')
+SMW = VSG().jav_Open(SMW_IP,OFileCSV)  #Create SMW Object
+FSW = VSA().jav_Open(FSW_IP,OFileCSV)  #Create FSW Object
 
 ##########################################################
 ### Instrument Settings
@@ -45,13 +45,13 @@ if Mixer:                                 #Mixer
 
 for freq in range(FreqStart,FreqStop,FreqStep):
     SMW.Set_Freq(freq)
-    time.sleep(0.5)
+    time.sleep(0.01)
     FSW.Set_Freq(freq)
     FSW.Set_InitImm()
     FSW.Set_Mkr_Peak()
     Mkr = FSW.Get_Mkr_XY()
-    OutStr = "%d,%s,%s"%(freq,Mkr[0],Mkr[1])
-    f.write(OutStr)
+    OFileCSV.write(f'{freq},{Mkr[0]},{Mkr[1]}')
+    OFileXML.write(f'  <Point x="{Mkr[0]}" y="{Mkr[1]}"/>')
 
 SMW.jav_ClrErr()                          #Clear Errors
 FSW.jav_ClrErr()                          #Clear Errors
