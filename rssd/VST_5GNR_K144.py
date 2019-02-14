e8805d0b5ce33b7bcc909ee8d4a8b77cefa93d05
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-02-14 14:06:08 -0800

    Moved methods into alphebetical order.

--- rssd/VST_5GNR_K144.py
+++ rssd/VST_5GNR_K144.py
@@ -24,57 +24,6 @@ class VST(object):
       self.NR_RBO   = 0         #RB Offset
       self.NR_Mod   = 'QAM64'   #QPSK; QAM16; QAM64; QAM256; PITB
 
-   def jav_Open(self,SMW_IP,FSW_IP,OFile=''):
-      self.SMW = VSG().jav_Open(SMW_IP,OFile)  #Create SMW Object
-      self.FSW = VSA().jav_Open(FSW_IP,OFile)  #Create FSW Object
-      return self
-
-   def jav_Close(self):
-      self.SMW.jav_Close()
-      self.FSW.jav_Close() 
-
-   def Set_5GNR_All(self):
-      try:
-         ### SMW Settings
-         self.SMW.Set_Freq(self.Freq)
-         self.SMW.Set_5GNR_BBState('OFF')
-         self.SMW.Set_5GNR_Direction(self.NR_Dir)
-         self.SMW.Set_5GNR_FreqRange(self.NR_Deploy)
-         self.SMW.Set_5GNR_ChannelBW(self.NR_ChBW)
-         self.SMW.Set_5GNR_BWP_SubSpace(self.NR_SubSp)
-         self.SMW.Set_5GNR_BWP_ResBlock(self.NR_RB)
-         self.SMW.Set_5GNR_BWP_ResBlockOffset(self.NR_RBO)
-         self.SMW.Set_5GNR_BWP_Ch_ResBlock(self.NR_RB)
-         self.SMW.Set_5GNR_BWP_Corset_ResBlock(self.NR_RB)
-         #self.SMW.Set_5GNR_BWP_Ch_ResBlockOffset(NR_RBO)
-         self.SMW.Set_5GNR_BWP_Ch_Modulation(self.NR_Mod)
-         self.SMW.Set_5GNR_SSB()
-         self.SMW.Set_5GNR_BBState('ON')
-         self.SMW.Set_RFState('ON')                     #Turn RF Output on
-         self.SMW.Set_RFPwr(self.SWM_Out)                    #Output Power
-      except:
-         print("NR5G_SetSettings: SMW Error")
-
-      try:
-         ### FSW Setting
-         self.FSW.Set_Freq(self.Freq)
-         self.FSW.Init_5GNR()
-         self.FSW.Set_5GNR_Direction(self.NR_Dir)
-         self.FSW.Set_5GNR_FreqRange(self.NR_Deploy)
-         self.FSW.Set_5GNR_ChannelBW(self.NR_ChBW)
-         self.FSW.Set_5GNR_BWP_SubSpace(self.NR_SubSp)
-         self.FSW.Set_5GNR_BWP_ResBlock(self.NR_RB)
-         self.FSW.Set_5GNR_BWP_ResBlockOffset(self.NR_RBO)
-         self.FSW.Set_5GNR_BWP_Ch_ResBlock(self.NR_RB)
-         self.FSW.Set_5GNR_BWP_Corset_ResBlock(self.NR_RB)
-         #self.FSW.Set_5GNR_BWP_Ch_ResBlockOffset(self.NR_RBO)
-         self.FSW.Set_5GNR_BWP_Ch_Modulation(self.NR_Mod)
-         self.FSW.Set_SweepCont(1)
-         self.FSW.Set_InitImm()
-      except:
-         print("NR5G_SetSettings: FSW Error")
-      return 0
-
    def Get_5GNR_All(self):
       DMRS = 0
       
@@ -182,6 +131,7 @@ class VST(object):
       print('SMW/FSW Values: %d %d'%(len(odata[2]),len(odata[2]))) 
 
       return odata
+
    def Get_5GNR_All_print(self):
       data = self.Get_5GNR_All()
       for i in range(len(data[0])):
@@ -192,6 +142,62 @@ class VST(object):
                print("%s\t%s\t%s"%(data[0][i],data[1][i],'<notRead>'))
             except:
                print("%s\t%s\t%s"%(data[0][i],'<notRead>',data[2][i]))
+
+   def jav_Open(self,SMW_IP,FSW_IP,OFile=''):
+      self.SMW = VSG().jav_Open(SMW_IP,OFile)  #Create SMW Object
+      self.FSW = VSA().jav_Open(FSW_IP,OFile)  #Create FSW Object
+      return self
+
+   def jav_Close(self):
+      self.SMW.jav_Close()
+      self.FSW.jav_Close() 
+   
+   def jav_Clear(self):
+      self.SMW.jav_Clear()
+      self.FSW.jav_Clear() 
+
+   def Set_5GNR_All(self):
+      try:
+         ### SMW Settings
+         self.SMW.Set_Freq(self.Freq)
+         self.SMW.Set_5GNR_BBState('OFF')
+         self.SMW.Set_5GNR_Direction(self.NR_Dir)
+         self.SMW.Set_5GNR_FreqRange(self.NR_Deploy)
+         self.SMW.Set_5GNR_ChannelBW(self.NR_ChBW)
+         self.SMW.Set_5GNR_BWP_SubSpace(self.NR_SubSp)
+         self.SMW.Set_5GNR_BWP_ResBlock(self.NR_RB)
+         self.SMW.Set_5GNR_BWP_ResBlockOffset(self.NR_RBO)
+         self.SMW.Set_5GNR_BWP_Ch_ResBlock(self.NR_RB)
+         self.SMW.Set_5GNR_BWP_Corset_ResBlock(self.NR_RB)
+         #self.SMW.Set_5GNR_BWP_Ch_ResBlockOffset(NR_RBO)
+         self.SMW.Set_5GNR_BWP_Ch_Modulation(self.NR_Mod)
+         self.SMW.Set_5GNR_SSB()
+         self.SMW.Set_5GNR_BBState('ON')
+         self.SMW.Set_RFState('ON')                     #Turn RF Output on
+         self.SMW.Set_RFPwr(self.SWM_Out)                    #Output Power
+      except:
+         print("NR5G_SetSettings: SMW Error")
+
+      try:
+         ### FSW Setting
+         self.FSW.Set_Freq(self.Freq)
+         self.FSW.Init_5GNR()
+         self.FSW.Set_5GNR_Direction(self.NR_Dir)
+         self.FSW.Set_5GNR_FreqRange(self.NR_Deploy)
+         self.FSW.Set_5GNR_ChannelBW(self.NR_ChBW)
+         self.FSW.Set_5GNR_BWP_SubSpace(self.NR_SubSp)
+         self.FSW.Set_5GNR_BWP_ResBlock(self.NR_RB)
+         self.FSW.Set_5GNR_BWP_ResBlockOffset(self.NR_RBO)
+         self.FSW.Set_5GNR_BWP_Ch_ResBlock(self.NR_RB)
+         self.FSW.Set_5GNR_BWP_Corset_ResBlock(self.NR_RB)
+         #self.FSW.Set_5GNR_BWP_Ch_ResBlockOffset(self.NR_RBO)
+         self.FSW.Set_5GNR_BWP_Ch_Modulation(self.NR_Mod)
+         self.FSW.Set_SweepCont(1)
+         self.FSW.Set_InitImm()
+      except:
+         print("NR5G_SetSettings: FSW Error")
+      return 0
+
 
 ##########################################################
 ### Instrument Settings
