59a524ceb55836aeafa760b92da76d73407a217b
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-02-14 16:42:59 -0800

    first tested prototype

--- rssd/VNA_Common.py
+++ rssd/VNA_Common.py
@@ -3,155 +3,146 @@
 ###
 ### Purpose: Vector Network Analyzer Common Functions
 ### Author : Martin C Lim
-### Date   : 2018.10.15
+### Date    : 2018.10.15
 ### Requird: python -m pip install pyvisa
-###  _____  _____   ____ _______ ____ _________     _______  ______ 
-### |  __ \|  __ \ / __ \__   __/ __ \__   __\ \   / /  __ \|  ____|
-### | |__) | |__) | |  | | | | | |  | | | |   \ \_/ /| |__) | |__   
-### |  ___/|  _  /| |  | | | | | |  | | | |    \   / |  ___/|  __|  
-### | |    | | \ \| |__| | | | | |__| | | |     | |  | |    | |____ 
-### |_|    |_|  \_\\____/  |_|  \____/  |_|     |_|  |_|    |______|
-###                         _            _           _ 
-###                        | |          | |         | |             
-###             _   _ _ __ | |_ ___  ___| |_ ___  __| |
-###            | | | | '_ \| __/ _ \/ __| __/ _ \/ _` |
-###            | |_| | | | | ||  __/\__ \ ||  __/ (_| |
-###             \__,_|_| |_|\__\___||___/\__\___|\__,_|
+###  _____  _____    ____ _______ ____ _________      _______  ______ 
+### |  __ \|  __ \ / __ \__    __/ __ \__    __\ \    / /  __ \|  ____|
+### | |__) | |__) | |  | | | | | |  | | | |    \ \_/ /| |__) | |__    
+### |  ___/|  _  /| |  | | | | | |  | | | |     \    / |  ___/|  __|  
+### | |     | | \ \| |__| | | | | |__| | | |      | |  | |     | |____ 
+### |_|     |_|  \_\\____/  |_|  \____/  |_|      |_|  |_|     |______|
 ###
 #####################################################################
-from rssd.yaVISA import jaVisa         # pylint: disable=E0611,E0401
+from rssd.yaVISA import jaVisa            # pylint: disable=E0611,E0401
 
 class VNA(jaVisa):
-   def __init__(self):
-      super(VNA,self).__init__()    #Python2/3
-      self.Model = "xxx"
-   
-   #####################################################################
-   ### VNA GET Functions Alphabetical
-   #####################################################################
-   def Get_Trace_Names(self,dChan=1):
-      rdStr = self.query(":CALC%d:PAR:CAT?"%(dChan))
-      return rdStr
-      
-   def Get_Trace_Save(self,dChan,sFName):
-      self.write(":MMEM:STOR:TRAC:PORT %d,'%s.s4p',COMP,1,2,3,4"%(dChan,sFName))
-      #self.write(":MMEM:STOR:TRAC:PORT %d,'%s.s2p',COMP,1,2"%(dChan,sFName))
+    def __init__(self):
+        super(VNA,self).__init__()     #Python2/3
+        self.Model = "xxx"
+    
+    #####################################################################
+    ### VNA GET Functions Alphabetical
+    #####################################################################
+    def Get_Trace_Names(self,dChan=1):
+        rdStr = self.query(":CALC%d:PAR:CAT?"%(dChan))
+        return rdStr
+    
+    def Save_Cal(self, sFName, dChan=1):
+        #Save calibration to cal manager.
+        if not sFName.lower().endswith(".cal"):
+            sFName += ".cal"
+        self.write(f":MMEM:STOR:CORR {dChan},'{sFName}'")
 
-   #####################################################################
-   ### VNA SET Functions Alphabetical
-   #####################################################################
-   def Set_Cal_Group(self,sName,dChan=1):
-      #sName should end in '.cal'
-      if not sName.lower().endswith(".cal"):
-         sName += ".cal"
-      self.write(":MMEM:LOAD:CORR:RES %d,%s"%(dChan,sName))    #Resolve Cal Group
-      self.write(":MMEM:LOAD:CORR %s"%(sName))                 #Load cal group.
+    def Save_Screen(self,sFName):
+        self.write('HCOP:DEV:LANG BMP')
+        self.write("MMEM:NAME '%s.BMP'"%(sFName))
+        self.write('HCOP:DEST "MMEM"; :HCOP')
 
-   def Set_Cal_Group_Disolve(self):
-      self.write(":MMEM:LOAD:CORR:RES")
+    def Save_State(self,sFName):
+        self.write("MMEM:STOR:STAT 1,'%s'"%(sFName))
 
-   def Set_Call_Save(self, sName, dChan=1):
-      if not sName.lower().endswith(".cal"):
-         sName += ".cal"
-      self.write(f":MMEM:STOR:CORR {dChan},'{sName}'")
+    def Save_Trace_SxP(self,sFName,dChan=1):                        #MMM
+        self.write("MMEM:STOR:TRAC:CHAN %d,'%s.s2p'"%(dChan,sFName))
+        #self.write(":MMEM:STOR:TRAC:PORT %d,'%s.s2p',COMP,1,2"%(dChan,sFName))
 
-   def Set_Channel(self,dChan,sName=""):
-      ##################################################################
-      ### SANALYZER, IQ, PNOISE, NOISE, Spur, ADEM, V5GT, LTE, OFDMVSA
-      ##################################################################
-      if sName == "":
-         sName = dChan
-      ChList = self.query(":CALC%d:PAR:CAT?"%(dChan)).split(",")
-      #print("Chan:%s in %s"%(Chan,ChList))
-      if ("'%s'"%sName) in ChList:
-         pass
-      else:
-         self.query(":INST:CRE %s,'%s';*OPC?"%(dChan,sName))
-      self.query(":INST:SEL '%s';*OPC?"%sName)
-      
-   def Set_FreqStart(self,fFreq,dChan=1):
-      self.write(":SENS%d:FREQ:STAR %f"%(dChan,fFreq))
+    def Save_Trace_CSV(self,sFName,dChan=1):
+        self.write("MMEM:STOR:TRAC:CHAN %d,'%s.csv'"%(dChan,sFName))
 
-   def Set_FreqStep(self,fFreq,dChan=1):
-      self.write(":SENS%d:FREQ:STEP %f"%(dChan,fFreq))   #RF Freq
+    #####################################################################
+    ### VNA SET Functions Alphabetical
+    #####################################################################
+'''
+    def Set_Cal_Group(self,sName,dChan=1):                          #MMM
+        #sName should end in '.cal'
+        if not sName.lower().endswith(".cal"):
+            sName += ".cal"
+        self.write(":MMEM:LOAD:CORR:RES %d,%s"%(dChan,sName))       #Resolve Cal Group
+        self.write(":MMEM:LOAD:CORR %s"%(sName))                    #Load cal group.
+'''
 
-   def Set_FreqStop(self,fFreq,dChan=1):
-      self.write(":SENS%d:FREQ:STOP %f"%(dChan,fFreq))   #RF Freq
+    def Set_FreqStart(self,fFreq,dChan=1):
+        self.write(":SENS%d:FREQ:STAR %f"%(dChan,fFreq))
 
-   def Set_IFBW(self,fFreq,dChan=1):
-      self.write("SENS%d:BAND %f"%(dChan,fFreq))
-      
-   def Set_InitImm(self,dChan=1):
-      self.query("INIT%d:IMM*OPC?"%(dChan))
+    def Set_FreqStop(self,fFreq,dChan=1):
+        self.write(":SENS%d:FREQ:STOP %f"%(dChan,fFreq))            #RF Freq
+
+    def Set_IFBW(self,fFreq,dChan=1):
+        self.write("SENS%d:BAND %f"%(dChan,fFreq))
         
-   def Set_PowerStart(self,fPwr,dChan=1):
-      self.write(":SOUR%d:POW:STAR %f dBm"%(dChan,fPwr))
+    def Set_InitImm(self):                                        
+        self.query("INIT:IMM;*OPC?")
+          
+    def Set_PowerStart(self,fPwr,dChan=1):
+        self.write(":SOUR%d:POW:STAR %f dBm"%(dChan,fPwr))
 
-   def Set_PowerStep(self,fPwr,dChan=1):
-      self.write(":SOUR%d:POW:STEP %f dBm"%(dChan,fPwr))
+    def Set_PowerStop(self,fPwr,dChan=1):
+        self.write(":SOUR%d:POW:STOP %f dBm"%(dChan,fPwr))
 
-   def Set_PowerStop(self,fPwr,dChan=1):
-      self.write(":SOUR%d:POW:STOP %f dBm"%(dChan,fPwr))
+    def Set_SweepCont(self,iON):
+        if iON == 1:
+            self.write("INIT:CONT ON")                          #Continuous Sweep
+        else:
+            self.write("INIT:CONT OFF")                         #Single Sweep
 
-   def Set_SweepCont(self,iON,dChan=1):
-      if iON == 1:
-         self.write("INIT%d:CONT ON")                    #Continuous Sweep
-      else:
-         self.write("INIT%d:CONT OFF")                   #Single Sweep
+    def Set_SweepPoints(self,dPoints,dChan=1):
+        self.write(":SENS%d:SWE:POIN %d"%(dChan,dPoints))  #RF Freq
 
-   def Set_SweepPoints(self,dPoints,dChan=1):
-      self.write(":SENS%d:SWE:POIN %d"%(dChan,dPoints))  #RF Freq
+    def Set_SweepTime(self,fSwpTime,dChan=1):
+        if fSwpTime == 0:
+            self.write("SENS%d:SWE:TIME:AUTO ON"%(dChan))  #Auto        
+        else:
+            self.write("SENS%d:SWE:TIME %f"%(dChan,fSwpTime))  #Sweep/Capture Time
 
-   def Set_SweepTime(self,fSwpTime,dChan=1):
-      self.write("SENS%d:SWE:TIME %f"%(dChan,fSwpTime))  #Sweep/Capture Time
+    def Set_Trace_Avg(self,state,dChan=1):
+        if state == 1:
+            self.write("SENS%d:AVER:STAT ON"%(dChan))
+        else:
+            self.write("SENS%d:AVER:STAT OFF"%(dChan))
 
-   def Set_Trace_Add(self,dChan=1):
-      rdStr = self.write("CALC%d:PAR:SDEF")
-      return rdStr
-      
-   def Set_Trace_Avg(self,sType,dChan=1):
-      self.write("DISP:TRAC%d:MODE AVER"%dChan)
-      self.write("SENS:DET1:FUNC AVER")
-      self.write("SENS:AVER:TYPE %s"%sType)  #LIN|VID
+    def Set_Trace_AvgCount(self,iAvg,dChan=1):
+        self.write("SENS%d:AVER:COUN %d"%(dChan,iAvg))
 
-   def Set_Trace_AvgCount(self,iAvg,dChan=1):
-      self.write("SENS%d:SWE:COUN %d"%(dChan,iAvg))
+    def Set_Trace_DelAll(self):
+        self.write("CALC:PAR:DEL:ALL")
 
-   def Set_Trace_DelAll(self,sMeas,dChan=1):
-      self.write("CALC:PAR:DEL:ALL")
+    def Set_Trace_MeasAdd(self,sMeas,dChan=1):
+        # S11/S21/S12/S22 ..... Sxxyy
+        # Y11/Y21/Y12/Y22 ..... Yxxyy
+        # A1D2/A1D4/A2D1  ..... A<port>G<port>
+        # B1D2/B1D4/B2D1  ..... B<port>G<port>
+        # IP3UI/IP3UO      ..... IP<order:3|5|7|9><side:U|L><DUT:I|O>
+        self.write(f"CALC{dChan}:PAR:SDEF '{sMeas}','{sMeas}'")     #<TrcName>,<Measurement>
+        self.write(f'DISP:WIND1:TRAC:EFE "{sMeas}"')                #Displays Trace
 
-   def Set_Trace_MeasAdd(self,sMeas,dChan=1):
-      # S11/S21/S12/S22 ..... Sxxyy
-      # Y11/Y21/Y12/Y22 ..... Yxxyy
-      # A1G2/A1G4/A2G1  ..... A<port>G<port>
-      # B1G2/B1G4/B2G1  ..... B<port>G<port>
-      # IP3UI/IP3UO     ..... IP<order:3|5|7|9><side:U|L><DUT:I|O>
-      sTrcName = self.Set_Trace_Add()
-      self.write("CALC%d:PAR:SDEF '%s','%s'"%(dChan,sTrcName,sMeas))
+    def Set_Trace_MeasAdd_AWave(self,APort,GenPort,dChan=1):
+        #Default: SAM; RMS; PEAK; AVG
+        self.Set_Trace_MeasAdd(f'A{APort}D{GenPort}RMS')
 
-   def Set_Trace_MeasAdd_AWave(self,APort,GenPort,dChan=1):
-      self.Set_Trace_MeasAdd("A%dG%d"%(APort,GenPort))
+    def Set_Trace_MeasAdd_BWave(self,BPort,GenPort,dChan=1):
+        self.Set_Trace_MeasAdd(f'B{BPort}D{GenPort}RMS')
 
-   def Set_Trace_MeasAdd_BWave(self,BPort,GenPort,dChan=1):
-      self.Set_Trace_MeasAdd("B%dG%d"%(BPort,GenPort))
+    def Set_Trace_MeasAdd_SParam(self,Port1,Port2,dChan=1):
+        self.Set_Trace_MeasAdd(f'S{Port1}{Port2}')
 
-   def Set_Trace_MeasAdd_SParam(self,Port1,Port2,dChan=1):
-      self.Set_Trace_MeasAdd("S%d%d"%(Port1,Port2))
+    def Set_Trace_MeasAdd_PwrMtr(self,GenPort,dChan=1):
+        self.Set_Trace_MeasAdd(f'Pmtr{1}D{GenPort}')
 
-   def Set_Trace_MeasAdd_IMD3(self,dChan=1):
-      self.write("SENS%d:FREQ:IMOD:ORD3 ON"%(dChan))
-      self.Set_Trace_MeasAdd("IP3UI"%(dChan))
-      self.Set_Trace_MeasAdd("IP3LI"%(dChan))
+'''
+    def Set_Trace_MeasAdd_IMD3(self,dChan=1):                         #mmm
+        self.write("SENS%d:FREQ:IMOD:ORD3 ON"%(dChan))
+        self.Set_Trace_MeasAdd("IP3UI")
+        self.Set_Trace_MeasAdd("IP3LI")
+'''
 
-   def Set_Trace_MeasDel(self,sTrcName,dChan=1):
-      self.write("CALC%d:PAR:DEL '%s'"%(dChan,sTrcName))
+    def Set_Trace_MeasDel(self,sTrcName,dChan=1):
+        self.write("CALC%d:PAR:DEL '%s'"%(dChan,sTrcName))
 
 #####################################################################
 ### Run if Main
 #####################################################################
 if __name__ == "__main__":
-   # this won't be run when imported
-#   ZVA = VNA().jav_openvisa('TCPIP0::192.168.1.131::5025::SOCKET')
-   ZVA = VNA().jav_Open('192.168.1.131')
-   print(ZVA.Get_Trace_Names())
-   ZVA.jav_Close()
+    # this won't be run when imported
+    ZVA = VNA().jav_openvisa('TCPIP0::192.168.1.31::inst0')
+    ZVA.Set_Trace_MeasAdd_IMD3()
+    print(ZVA.Get_Trace_Names())
+    ZVA.jav_Close()
