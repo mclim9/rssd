750bbe2741c5ad3cb7430602da3b4dfe1d858984
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-02-27 11:34:44 -0800

    Moved user Entry to top. Moved 5GNR setup outside of loop Add CCDF

--- rssd/examples/VST_5GNR_EVM.py
+++ rssd/examples/VST_5GNR_EVM.py
@@ -11,10 +11,16 @@
 ##########################################################
 SMW_IP   = '192.168.1.114'
 FSW_IP   = '192.168.1.109'
+Freq    = 40e9
+pwrArry  = [-20,-10,-5,0,5]
+NR_Dir  = 'UL'
+NR_ChBW = 400
+NR_RB   = 264           #100:060:132  200:060:264  <Not Appli>
+                        #100:120:066  200:120:132  400:120:264
 subCarr  = [120]
 modArry  = ['QPSK', 'QAM64'] #QPSK; QAM16; QAM64; QAM256; PITB
-pwrArry  = [-20,-10,-5,0,5]
 numMeas  = 1
+AutoLvl  = 1
 
 ##########################################################
 ### Code Overhead: Import and create objects
@@ -29,40 +35,45 @@ OFile = FileIO().makeFile(__file__)
 ### Code Start
 ##########################################################
 NR5G = VST().jav_Open(SMW_IP,FSW_IP,OFile)
-NR5G.NR_Dir  = 'UL'
-NR5G.NR_ChBW = 400
-NR5G.NR_RB   = 264      #100:060:132  200:060:264  <Not Appli>
-                        #100:120:066  200:120:132  400:120:264
-NR5G.Freq    = 40e9
+NR5G.NR_Dir  = NR_Dir
+NR5G.NR_ChBW = NR_ChBW
+NR5G.NR_RB   = NR_RB
+NR5G.Freq    = Freq
 
 ##########################################################
 ### Measure Time
 ##########################################################
 #sDate = datetime.now().strftime("%y%m%d-%H:%M:%S.%f") #Date String
-OFile.write('EVM,ChBW,SubSp,Mod,Pwr,SubFram,Iter,CmdTime')
+OFile.write('AutoLvl,EVM,ChBW,SubSp,Mod,Pwr,SubFram,Iter,CmdTime,Attn,Preamp,RefLvl')
 
-for mod in modArry:
-   for subC in subCarr:
+NR5G.FSW.Init_5GNR()
+NR5G.FSW.Set_5GNR_EVMUnit('DB')
+NR5G.FSW.Set_Trig1_Source('EXT')
+NR5G.FSW.Set_SweepCont(0)
+
+for mod in modArry:                       #Loop: Modulation
+   for subC in subCarr:                   #Loop: Subcarrier
       NR5G.NR_SubSp = subC
       NR5G.NR_Mod   = mod
-      NR5G.Set_5GNR_All()              #Create Waveform
-      NR5G.FSW.Set_SweepCont(0)
-      NR5G.FSW.Set_5GNR_EVMUnit('DB')
-      NR5G.FSW.Set_Trig1_Source('EXT')
+      NR5G.Set_5GNR_All()                 #Create Waveform
       print(f'RFBW:{NR5G.NR_ChBW} SubC:{subC} Mod:{mod}')
-      for pwr in pwrArry:
+      for pwr in pwrArry:                 #Loop: Power
+         NR5G.FSW.Init_5GNR()
          NR5G.SMW.Set_RFPwr(pwr)
-         NR5G.FSW.Set_Autolevel()
-         for subFram in [10]:
+         if AutoLvl:
+            NR5G.FSW.Set_Autolevel()
+         else:
+            NR5G.FSW.Set_5GNR_AutoEVM()
+         for subFram in [10]:             #Loop: Subframe
             NR5G.FSW.Set_SweepTime((subFram)*1.1e-3)
             NR5G.FSW.Set_5GNR_SubFrameCount(subFram)
-            for i in range(numMeas):
+            for i in range(numMeas):      #Loop: # of Measurements
                tick = datetime.now()
                NR5G.FSW.Set_InitImm()
                EVM = NR5G.FSW.Get_5GNR_EVM()
+               Attn = NR5G.FSW.Get_AmpSettings()
                d = datetime.now() - tick
-#               OutStr = '%f,%d,%d,%s,%d,%2d,%3d.%06d'%(EVM,ChBW,subC,mod,subFram,i,d.seconds,d.microseconds)
-               OutStr = f'{EVM},{NR5G.NR_ChBW},{subC},{mod},{pwr},{subFram},{i},{d.seconds},{d.microseconds}'
+               OutStr = f'{AutoLvl},{EVM:.3f},{NR5G.NR_ChBW},{subC},{mod},{pwr},{subFram},{i},{d.seconds}.{d.microseconds:.4f},{Attn}'
                OFile.write (OutStr)
    
 ##########################################################
