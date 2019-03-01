e25b3bc6848e1a76d7383e904ace1aadbd4f4a85
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-03-01 11:56:37 -0800

    no IDN print

--- rssd/VST_5GNR_K144.py
+++ rssd/VST_5GNR_K144.py
@@ -144,8 +144,8 @@ class VST(object):
                print("%s\t%s\t%s"%(data[0][i],'<notRead>',data[2][i]))
 
    def jav_Open(self,SMW_IP,FSW_IP,OFile=''):
-      self.SMW = VSG().jav_Open(SMW_IP,OFile)  #Create SMW Object
-      self.FSW = VSA().jav_Open(FSW_IP,OFile)  #Create FSW Object
+      self.SMW = VSG().jav_Open(SMW_IP,OFile,prnt=0)  #Create SMW Object
+      self.FSW = VSA().jav_Open(FSW_IP,OFile,prnt=0)  #Create FSW Object
       return self
 
    def jav_Close(self):
