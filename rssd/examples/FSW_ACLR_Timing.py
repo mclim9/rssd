64c4d5b7d975c6fb70dd0a63fb97ceafa93d4576
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-04-09 09:38:58 -0700

    add sweep mode/optimization parameters add sweep param and attenuation param output

--- rssd/examples/FSW_ACLR_Timing.py
+++ rssd/examples/FSW_ACLR_Timing.py
@@ -9,17 +9,19 @@
 ##########################################################
 ### User Entry
 ##########################################################
-VSA_IP  = '192.168.1.108'
-VSG_IP  = '192.168.1.116' 
+VSA_IP  = '192.168.1.109'
+VSG_IP  = '192.168.1.114' 
 MeasTim = 10e-3
 Freq    = 2.3e9
 RBW     = 20e3
 ChBW    = 18e6
 ChSpace = 20e6
 Avg     = 0
+SweMode = 'AUTO'            #AUTO | SPEed | DYN
+SweType = 'AUTO'           #AUTO | SWE | FFT
 
 ### Loops
-Repeat  = 4
+Repeat  = 10
 PwrSweep = 59
 
 ##########################################################
@@ -47,9 +49,11 @@ VSA.Set_ACLR_NumAdj(2)
 VSA.Set_ResBW(RBW)
 #VSA.Set_DisplayUpdate("OFF")
 VSA.Set_SweepTime(MeasTim)
-VSA.Set_Trace_Avg('lin')
+VSA.Set_Trace_Avg('LIN')
 VSA.Set_Trace_AvgCount(Avg)
 VSA.Set_Trace_Detector('RMS')
+VSA.Set_SweepOpt(SweMode)
+VSA.Set_SweepType(SweType)
 VSA.Set_YIG('OFF')
 if 0:
     VSA.Set_Trig1_Source('Ext')
@@ -58,12 +62,11 @@ if 0:
 ### Measure Time
 ##########################################################
 #sDate = datetime.now().strftime("%y%m%d-%H:%M:%S.%f") #Date String
-OFile.write('Iter,RBW,SwpTime,SMWPwr,AL Time, TotalTime,TxPwr,Adj-,Adj+,Alt-,Alt+')
+OFile.write('Iter,RBW,SwpTime,SMWPwr,AL Time,TotalTime,Attn,PreAmp,RefLvl,SwpTime,SwpPts,SwpType,SwpOpt,TxPwr,Adj-,Adj+,Alt-,Alt+')
 for i in range(Repeat):
     for pwr in range(PwrSweep):
         tick = datetime.now()
         ### <\thing we are timing>
-        # rdStr = VSG.query('*IDN?')
         VSG.write(f':POW:AMPL {-50 + pwr}dbm')                  ### VSG Power
 
         #################
@@ -85,12 +88,14 @@ for i in range(Repeat):
         tockA =  datetime.now()
         VSA.write(':INIT:CONT OFF')                             # Single Sweep
         VSA.query(':INIT:IMM;*OPC?')                            # Take Sweep
-        rdStr = VSA.query(':CALC:MARK:FUNC:POW:RES? MCAC')
+        ACLR = VSA.query(':CALC:MARK:FUNC:POW:RES? MCAC')
         ### <\thing we are timing>
         tockB = datetime.now()
+        SwpParam = VSA.Get_SweepParams()
+        AmpSet  = VSA.Get_AmpSettings()
         ALTime = tockA - tick
         TotTime = tockB - tick
-        OutStr = f'{i},{RBW},{MeasTim},{-50+pwr},{ALTime.seconds:3d}.{ALTime.microseconds:06d},{TotTime.seconds:3d}.{TotTime.microseconds:06d},{rdStr}'
+        OutStr = f'{i},{RBW},{MeasTim},{-50+pwr},{ALTime.seconds:3d}.{ALTime.microseconds:06d},{TotTime.seconds:3d}.{TotTime.microseconds:06d},{AmpSet},{SwpParam},{ACLR}'
         OFile.write (OutStr)
 
 ##########################################################
