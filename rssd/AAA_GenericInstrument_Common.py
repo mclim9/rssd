7f7ceeeff575de5cb854d1d786f83bef4b9a2895
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-04-19 16:36:00 -0700

    Chg 3 to 4 space tab

--- rssd/AAA_GenericInstrument_Common.py
+++ rssd/AAA_GenericInstrument_Common.py
@@ -1,65 +1,65 @@
-#####################################################################
+###############################################################################
 ### Rohde & Schwarz Automation for demonstration use.
-###
 ### Purpose: AAA Common Functions
 ### Author : Martin C Lim
 ### Date   : 20xx.xx.xx
 ###  _____  _____   ____ _______ ____ _________     _______  ______ 
 ### |  __ \|  __ \ / __ \__   __/ __ \__   __\ \   / /  __ \|  ____|
-### | |__) | |__) | |  | | | | | |  | | | |   \ \_/ /| |__) | |__   
+### | |__) | |__) | |  | | | | | |  | | | |   \ \_/ /| |__) | |__    
 ### |  ___/|  _  /| |  | | | | | |  | | | |    \   / |  ___/|  __|  
 ### | |    | | \ \| |__| | | | | |__| | | |     | |  | |    | |____ 
 ### |_|    |_|  \_\\____/  |_|  \____/  |_|     |_|  |_|    |______|
 ###                         _            _           _ 
-###                        | |          | |         | |             
+###                        | |          | |         | |
 ###             _   _ _ __ | |_ ___  ___| |_ ___  __| |
 ###            | | | | '_ \| __/ _ \/ __| __/ _ \/ _` |
 ###            | |_| | | | | ||  __/\__ \ ||  __/ (_| |
 ###             \__,_|_| |_|\__\___||___/\__\___|\__,_|
 ###
-#####################################################################
+###############################################################################
 from rssd.yaVISA import jaVisa
 
 class AAA(jaVisa):
-   def __init__(self):
-      super(AAA, self).__init__()
-      self.Model = "AAA"
-      
-   #####################################################################
-   ### AAA Functions Alphabetical
-   #####################################################################
-   def Get_ACLR(self):
-      ACLR = self.query(':CALC:MARK:FUNC:POW:RES? MCAC').split(',')
-      return ACLR
+    def __init__(self):
+        super(AAA, self).__init__()
+        self.Model = "AAA"
 
-   def Get_ChPwr(self):
-      out = self.queryFloat('FETC:SUMM:POW?')
-      return out 
+    #####################################################################
+    ### AAA Get Functions
+    #####################################################################
+    def Get_ACLR(self):
+        rdStr = self.query(':CALC:MARK:FUNC:POW:RES? MCAC').split(',')
+        return rdStr
 
-   def Get_Channels(self):
-      ChList = self.query('INST:LIST?').split(',')
-      return(ChList)
-      
-   def Init_Measurement(self):
-      #Configure instrument measurment
-      
-   def Set_DisplayUpdate(self,state):
-      # Param: ON|OFF
-      self.write('SYST:DISP:UPD %s'%state);     #Display Update State
-       
-   def Set_Freq(self,fFreq):
-      self.write(':SENS:FREQ:CENT %.0f HZ'%fFreq);         #RF Freq
+    def Get_ChPwr(self):
+        rdStr = self.queryFloat('FETC:SUMM:POW?')
+        return rdStr 
 
-   def Set_VidBW(self,fFreq):
-      if fFreq == 0:
-         self.write(':SENS:BAND:VID:AUTO ON');
-      else:
-         self.write(':SENS:BAND:VID %f'%fFreq);
-      
-#####################################################################
-### Run if Main  (Won't run when imported)
-#####################################################################
+    def Get_Channels(self):
+        rdStr = self.query('INST:LIST?').split(',')
+        return rdStr
+
+    #####################################################################
+    ### AAA Init Functions
+    #####################################################################
+    def Init_Measurement(self):
+        #Configure instrument measurment
+        pass
+
+    #####################################################################
+    ### AAA Set Functions
+    #####################################################################
+    def Set_DisplayUpdate(self,state):
+        # Param: ON|OFF
+        self.write(f'SYST:DISP:UPD {state}')            #Display Update State
+
+    def Set_Freq(self,fFreq):
+        self.write(f':SENS:FREQ:CENT {fFreq}')          #RF Freq
+
+###############################################################################
+### Debug Main.  Won't run when imported
+###############################################################################
 if __name__ == "__main__":
-   AAA_Inst = AAA().jav_Open("192.168.1.100")
-   AAA_Inst.jav_IDN()
-   AAA_Inst.jav_ClrErr()
+    AAA_Inst = AAA().jav_Open("192.168.1.100")
+    AAA_Inst.jav_IDN()
+    AAA_Inst.jav_ClrErr()
