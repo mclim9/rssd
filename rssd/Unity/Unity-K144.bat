23d8e6d2e32de7d5fcc5d32e9215ecd8f7d452ce
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-05-13 16:17:42 -0700

    delete unneeded files

--- rssd/Unity/Unity-K144.bat
+++
@@ -1,4 +0,0 @@
-REM python -OO -m PyInstaller -a --onefile --icon=Unity.ico --version-file version.txt --paths "C:\Program Files (x86)\Python37-32\DLLs\dllls" Unity-K144.py 
-
-python -OO -m PyInstaller -a --icon=Unity.ico --version-file version.txt --paths "C:\Program Files (x86)\Python37-32\DLLs\dllls" Unity-K144.py 
-
