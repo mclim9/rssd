7b608414780be334ba5a8d9bfdf3256bd2e44c80
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-05-17 15:58:39 -0700

    simple demo code.

--- __main__.py
+++ __main__.py
@@ -1,1 +1,25 @@
-import examples.VSE_MultiCC_K96
+##########################################################
+### User Entry
+##########################################################
+instru_ip  = '192.168.1.114'
+
+##########################################################
+### Code Overhead: Import and create objects
+##########################################################
+from rssd.yaVISA_socket import jaVisa
+from rssd.FileIO        import FileIO
+
+OFile = FileIO().makeFile(__file__)
+instr = jaVisa().jav_Open(instru_ip,OFile)  #Create Object
+
+##########################################################
+### Code Start
+##########################################################
+instr.write('*CLS;*WAI')
+rdStr = instr.query('*IDN?')
+OFile.write(rdStr)
+
+##########################################################
+### Cleanup Automation
+##########################################################
+instr.jav_Close()
