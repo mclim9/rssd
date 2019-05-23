942307b56b3fbf2dd93dec9f8bd59c5066e4bfe6
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-05-23 13:22:40 -0700

    :sparkles: comment cleanup

--- rssd/examples/AAA_CommandTime.py
+++ rssd/examples/AAA_CommandTime.py
@@ -1,44 +1,43 @@
-##########################################################
+###############################################################################
 ### Rohde & Schwarz Automation for demonstration use.
-###
 ### Title  : Timing SCPI Commands Example
-### Author : mclim
-### Date   : 2018.05.24
-### Steps  : 
-###
-##########################################################
+### Creatd : mclim, 2018.05.24
+###############################################################################
 ### User Entry
-##########################################################
-instru_ip  = '192.168.1.114'
+###############################################################################
+instru_ip  = '192.168.1.109'
 
-##########################################################
+###############################################################################
 ### Code Overhead: Import and create objects
-##########################################################
+###############################################################################
 from rssd.yaVISA_socket import jaVisa
 from rssd.FileIO        import FileIO
 from datetime           import datetime
 
-OFile = FileIO().makeFile(__file__)
-#instr = jaVisa().jav_openvisa(f'TCPIP0::{instru_ip}::inst0',OFile)  #Create VISA Object
-instr = jaVisa().jav_Open(instru_ip,OFile)            #Create Object
+OFile = FileIO().makeFile(__file__)                     # Create File
+instr = jaVisa().jav_Open(instru_ip,OFile)              # Instrument Object
 
-##########################################################
+###############################################################################
 ### Code Start
-##########################################################   
-#sDate = datetime.now().strftime("%y%m%d-%H:%M:%S.%f") #Date String
-OFile.write('Iter,CmdTime,Response')
+###############################################################################
+sDate = datetime.now().strftime("%y%m%d-%H:%M:%S")      #Date String
+OFile.write('Date,Iter,CmdTime,Response')
 
+ALTime = []
 for i in range(10):
-   tick = datetime.now()
-   ### <\thing we are timing>
-   rdStr = instr.query('*IDN?')
-   ### <\thing we are timing>
-   tock = datetime.now()
-   ALTime = tock - tick
-   OutStr = f'{i},{ALTime.seconds:3d}.{ALTime.microseconds:06d},{rdStr}'
-   OFile.write (OutStr)
-   
-##########################################################
+    tick = datetime.now()
+    ### <\thing we are timing>
+    rdStr = instr.query('INIT:IMM;*OPC?')
+    ### <\thing we are timing>
+    tock = datetime.now()
+    a = tock - tick
+    ALTime.append(a.seconds + (a.microseconds/1e6))
+    OutStr = f'{sDate},{i},{ALTime[i]:.6f},{rdStr}'
+    OFile.write (OutStr)
+
+print(f'Avg time: {sum(ALTime) / float(len(ALTime))} secs')
+
+###############################################################################
 ### Cleanup Automation
-##########################################################
+###############################################################################
 instr.jav_Close()
