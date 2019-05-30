97885e4352353f5f9e5e202a2e177579d38d0bdd
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-05-30 15:31:27 -0700

    del datetime.  Already in FileIO add default INIT:IMM query chg tick tock calc to reduce lines

--- rssd/examples/AAA_CommandTime.py
+++ rssd/examples/AAA_CommandTime.py
@@ -5,40 +5,38 @@
 ###############################################################################
 ### User Entry
 ###############################################################################
-instru_ip  = '192.168.1.109'
+instru_ip  = '192.168.1.107'
 
 ###############################################################################
 ### Code Overhead: Import and create objects
 ###############################################################################
 from rssd.yaVISA_socket     import jaVisa
 from rssd.FileIO            import FileIO
-from datetime               import datetime
 import timeit
 
 OFile = FileIO().makeFile(__file__)
-instr = jaVisa().jav_Open(instru_ip,OFile)                #Create Object
+instr = jaVisa().jav_Open(instru_ip,OFile)                  #Create Object
 
 ###############################################################################
 ### Code Start
 ###############################################################################
-sDate = datetime.now().strftime("%y%m%d-%H:%M:%S")     #Date String
 OFile.write('Iter,CmdTime,Response')
 
 ALTime = []
 for i in range(10):
+    rdStr = ''
     tick = timeit.default_timer()
     ### <\thing we are timing>
+    if 1:
+        instr.query('INIT:IMM;*OPC?')
     # rdStr = instr.query(':SENS:ADJ:LEV;*OPC?')
-    instr.write('INIT:IMM;*WAI')
-    rdStr = instr.query('FETC:SUMM:EVM:ALL?')
+    # rdStr = instr.query('FETC:SUMM:EVM:ALL?')
     ### <\thing we are timing>
-    tock = timeit.default_timer()
-    a = tock - tick
-    ALTime.append(a)
-    OutStr = f'{sDate},{i},{ALTime[i]:.6f},{rdStr}'
+    ALTime.append(timeit.default_timer() - tick)
+    OutStr = f'{i},{ALTime[i]:.6f},{rdStr}'
     OFile.write (OutStr)
 
-print(f'Avg time: {sum(ALTime) / float(len(ALTime))} secs')
+print(f'Avg time  : {sum(ALTime) / float(len(ALTime))} secs')
 
 ###############################################################################
 ### Cleanup Automation
