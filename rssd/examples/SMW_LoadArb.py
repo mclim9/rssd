5047a22d25ececc0b1e57e29b71d3b5d3d534d1f
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-06-13 14:03:27 -0700

    :zap: updated comments

--- rssd/examples/SMW_LoadArb.py
+++ rssd/examples/SMW_LoadArb.py
@@ -15,10 +15,10 @@ host = '192.168.1.114'               #Get local machine name
 from rssd.SMW_Common import VSG
 
 SMW = VSG().jav_Open(host) 
-#SMW.jav_logSCPI()               #Log SCPI commands
-SMW.Set_Freq(10e9)               #Set 10GHz
-SMW.Set_RFPwr(-30)               #Output -30dBm
-SMW.Set_RFState('ON')            #Turn RF Output on
-SMW.Set_ArbWv('composer.wv')     #Load file
-SMW.Set_ArbState('ON')           #Turn on Arb & IQ Mod
-SMW.jav_ClrErr()                 #Clear Errors
+#SMW.jav_logSCPI()                  #Log SCPI commands
+SMW.Set_Freq(10e9)                  #Set 10GHz
+SMW.Set_RFPwr(-30)                  #Output -30dBm
+SMW.Set_RFState('ON')               #Turn RF Output on
+SMW.Set_ArbWv('composer.wv')        #Load file
+SMW.Set_ArbState('ON')              #Turn on Arb & IQ Mod
+SMW.jav_ClrErr                      #Clear Errors
