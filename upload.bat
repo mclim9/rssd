c88423dc18194e9bfac18ab544cfa0f727f761e4
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-12-10 16:36:38 -0800

    update for version 2019.12.0

--- upload.bat
+++ upload.bat
@@ -1,4 +1,4 @@
 python setup.py sdist
 python -m unittest -v test.test_RSSD
 python -m unittest -v test.test_yaVISA
-twine upload .\dist\rssd-2019.11.0.tar.gz 
+twine upload .\dist\rssd-2019.12.0.tar.gz 
