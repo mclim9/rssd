7dcb510ce706cd854d297784040f7f65eef14941
mclim-rsa23770 <martin_lim@rsa.rohde-schwarz.com>
2019-12-20 10:37:44 -0800

    Clean up & add Documentation

--- rssd/examples/SMW_SCPIWaveform.py
+++ rssd/examples/SMW_SCPIWaveform.py
@@ -1,42 +1,42 @@
-##########################################################
+###############################################################################
 ### Rohde & Schwarz Automation for demonstration use.
-###
-### Title  : SCPI Commands Example
+### Title  : Raw Binary to SMW
 ### Author : mclim
 ### Date   : 2018.10.26
-###
-##########################################################
+###############################################################################
 ### User Entry
-##########################################################
+###############################################################################
 SMW_IP   = '192.168.1.114'
-Freq     = 28e9
-ChBW     = 100
 
-##########################################################
+###############################################################################
 ### Code Overhead: Import and create objects
-##########################################################
+###############################################################################
 from rssd.VSG.Common    import VSG
 from rssd.FileIO        import FileIO
 
-SMW = VSG().jav_Open(SMW_IP,OFile)  #Create SMW Object
+SMW = VSG().jav_Open(SMW_IP)  #Create SMW Object
 
-##########################################################
+###############################################################################
 ### Code Start
-##########################################################
-# parts
-#scpi = "MMEM:DATA '/var/user/test.txt',#15hallo"
-#scpi = 'BB:ARB:WAV:DATA "test.wv",#14'    #bytes = 2(I)+2(Q)= 4bytes = 1IQ sample
-scpi  = ':MMEM:DATA:UNPR "NVWFM://var//user//wave.wv",#14'
-bits  = b'\x00\x01\x02\x03'
-cmd   = bytes(scpi, 'utf-8') + bits
+###############################################################################
+### :MMEM:DATA:UNPR "NVWFM://var//user//<wave.wv>",#<numSize><NumBytes><I0Q0...IxQx>
+###     wave.wv : Name of *.wv to be created
+###     numSize : Number of bytes in <NumBytes>
+###     NumBytes: Number of bytes to follow.
+###               Each I (or Q) value is two bytes
+###               I(2 bytes) + Q(2bytes) = 4 bytes/IQ pair
+###               NumBytes = NumIQPair * 4
+###
+scpi  = ':MMEM:DATA:UNPR "NVWFM://var//user//wave.wv",#14'      # Ascii Cmd
+bits  = b'\x00\x01\x02\x03'                                     # Binary Data
+cmd   = bytes(scpi, 'utf-8') + bits                             # Add ASCII + Bin
 print(bytes(cmd))
 SMW.K2.write_raw(cmd)
-SMW.write('SOUR1:BB:ARB:WAV:CLOC "/var/user/wave.wv",1.1E6')
-#SMW.write(':MMEM:DATA:UNPR "NVMKR:/var/user/wave.wv",#185*7uuf5*')
-SMW.write('BB:ARB:WAV:SEL "/var/user/wave.wv"')
+SMW.write('SOUR1:BB:ARB:WAV:CLOC "/var/user/wave.wv",1.1E6')    # Set Fs/Clk Rate
+SMW.write('BB:ARB:WAV:SEL "/var/user/wave.wv"')                 # Select Arb File
 print(SMW.query('SYST:ERR?'))
 
-##########################################################
+###############################################################################
 ### Close Nicely
-##########################################################
+###############################################################################
 SMW.jav_Close()
