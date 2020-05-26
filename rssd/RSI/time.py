6e91731cf3606f4df7381b46fa2f6a9508c84801
RSA23770 <trabin9@gmail.com>
2020-05-26 15:05:54 -0700

    automation time keeper

--- rssd/RSI/time.py
+++ rssd/RSI/time.py
@@ -6,34 +6,44 @@
 import timeit
 import time
 
-class timr():                                                       #pylint: disable=E0102
-    """ Rohde & Schwarz Vector Signal Analyzer 5GNR Object """
+class timer():                                                       #pylint: disable=E0102
     def __init__(self):
         self.ticks  = []
         self.n      = 0
 
-    def clear(self):
+    def start(self):
         self.ticks = []
+        self.tick()
 
     def tick(self):
         self.ticks.append(timeit.default_timer())
 
-    def getTimes(self):
-        deltaTimes = []
+    def deltaTimes(self):
+        deltaTimes  = []
+        maxIndx     = len(self.ticks)-1
         for i,tick in enumerate(self.ticks):
-            deltaTimes = self.ticks[i+1] - self.ticks[i]
+            if (i < maxIndx):
+                deltaTimes.append(self.ticks[i+1] - self.ticks[i])
+        deltaTimes.append(self.ticks[maxIndx] - self.ticks[0])
         return deltaTimes
 
+    def deltaTimeTxt(self):
+        outStr = ""
+        for each in self.deltaTimes():
+            outStr += f'{each:2,.3f},'
+        return outStr
 
 #####################################################################
 ### Run if Main
 #####################################################################
 if __name__ == "__main__":
     ### this won't be run when imported
-    asdf = timr()
+    asdf = timer()
     asdf.tick()
+    time.sleep(0.6)
+    asdf.start()
     time.sleep(1.2)
     asdf.tick()
-    asdf.getTimes()
+    adf =asdf.deltaTimeTxt()
     pass
     
