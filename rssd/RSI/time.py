609f96c24115b834cd5d73b8aefd5a4fff9f2c90
Martin Lim <trabin9@gmail.com>
2020-05-28 15:43:39 -0700

    add Hours until done functionality

--- rssd/RSI/time.py
+++ rssd/RSI/time.py
@@ -8,8 +8,11 @@ import time
 
 class timer():                                                       #pylint: disable=E0102
     def __init__(self):
-        self.ticks  = []
-        self.n      = 0
+        self.timeStart  = 0
+        self.numTest    = 100
+        self.curTest    = 1
+        self.ticks      = []
+        self.n          = 0
 
     def start(self):
         self.ticks = []
@@ -25,6 +28,13 @@ class timer():                                                       #pylint: di
             if (i < maxIndx):
                 deltaTimes.append(self.ticks[i+1] - self.ticks[i])
         deltaTimes.append(self.ticks[maxIndx] - self.ticks[0])
+
+        ### Calculate time left
+        remTest = self.numTest - self.curTest
+        deltaTime = (timeit.default_timer() - self.timeStart) / self.curTest
+        timeLeft = deltaTime * (remTest) / (60 * 60)
+        deltaTimes.append(timeLeft)     # in hours
+        self.curTest += 1
         return deltaTimes
 
     def deltaTimeTxt(self):
@@ -32,6 +42,10 @@ class timer():                                                       #pylint: di
         for each in self.deltaTimes():
             outStr += f'{each:2,.3f},'
         return outStr
+
+    def suite_start(self):
+        self.timeStart = timeit.default_timer()
+
 
 #####################################################################
 ### Run if Main
