0daf8322f2b8adc0f0fdc30f8bafd08ddbe6d923
Martin Lim <trabin9@gmail.com>
2020-05-29 20:30:05 -0700

    prep for 2020.6.0

--- setup.py
+++ setup.py
@@ -11,49 +11,50 @@
 ##########################################################
 ### Upload to PyPi
 ### python setup.py sdist           #Creates <pkg>.tar.gz
-### twine upload .\dist\rssd-0.1.8.tar.gz 
+### twine upload .\dist\rssd-0.1.8.tar.gz
 
-import os
+# import os
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
     long_description = f.read()
 
 setup(name='rssd',
-    version='2020.04.0',
-    description='Rohde & Schwarz SCPI Driver',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Development Status :: 4 - Beta',      #3:Alpha 4:Beta 5:Production/Stable
-        'License :: Freely Distributable',
-        'License :: Other/Proprietary License',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
-        'Topic :: System :: Hardware :: Hardware Drivers',
-    ],
-    keywords='Rohde Schwarz test equipment FSW FSV NRP NRQ OSP SGT SMA SMB SMBV SMW SCPI VSA VSG VST',
-    url='https://github.com/mclim9/rssd',
-    author='Martin Lim',
-    author_email='martin.lim@rsa.rohde-schwarz.com',
-    license='R&S Terms and Conditions for Royalty-Free Products',
-    packages=find_packages(exclude=['test','proto']),
-    #packages=['rssd'],
-    install_requires=[
-        'pyvisa>=1.9.0',
-        'future_fstrings>=1.0.0',
-    ],
-    test_suite = 'test',
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-          'rssd=rssd.bin.cli:main'
-        ],
-    },
-)
+      version='2020.06.0',
+      description='Rohde & Schwarz SCPI Driver',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 4 - Beta',      #3:Alpha 4:Beta 5:Production/Stable
+          'License :: Freely Distributable',
+          'License :: Other/Proprietary License',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+          'Topic :: System :: Hardware :: Hardware Drivers',
+      ],
+      keywords='Rohde Schwarz test equipment FSW FSV NRP NRQ OSP SGT SMA SMB SMBV SMW SCPI VSA VSG VST',
+      url='https://github.com/mclim9/rssd',
+      author='Martin Lim',
+      author_email='martin.lim@rsa.rohde-schwarz.com',
+      license='R&S Terms and Conditions for Royalty-Free Products',
+      packages=find_packages(exclude=['test','proto']),
+      #packages=['rssd'],
+      install_requires=[
+          'pyvisa>=1.9.0',
+          'future_fstrings>=1.0.0',
+      ],
+      test_suite = 'test',
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'rssd=rssd.bin.cli:main'
+              ],
+      },
+    )
--- upload.bat
+++ upload.bat
@@ -1,4 +1,4 @@
 python setup.py sdist
 python -m unittest -v test.test_RSSD
 python -m unittest -v test.test_yaVISA
-twine upload .\dist\rssd-2020.4.0.tar.gz 
+twine upload .\dist\rssd-2020.6.0.tar.gz 
