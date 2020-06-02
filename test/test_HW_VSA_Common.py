eb3ef9750932006851c7bddf7bb330240f4fe55b
Martin Lim <trabin9@gmail.com>
2020-06-02 11:47:31 -0700

    Add ACLR tests Add Attn methods Add FSW_Input Test Add FSW_Overload test

--- test/test_HW_VSA_Common.py
+++ test/test_HW_VSA_Common.py
@@ -31,8 +31,12 @@ class TestGeneral(unittest.TestCase):
 ### <Test>
 ###############################################################################
     def test_FSW_ACLR(self):
+        self.FSW.Init_ACLR()
+        self.FSW.Set_ACLR_AdjBW(95e6)
+        self.FSW.Set_ACLR_AdjSpace(100e6)
+        self.FSW.Set_ACLR_CHBW(100e6)
+        self.FSW.Set_ACLR_NumAdj(2)
         self.FSW.Get_ACLR()
-        #var = input("Please enter something: ")
 
     def test_FSW_Connect(self):
         self.FSW.jav_IDN()
@@ -51,8 +55,11 @@ class TestGeneral(unittest.TestCase):
         self.FSW.Set_ResBW(1e6)
         self.FSW.Set_VidBW(1e6)
         self.FSW.Set_Span(100e6)
+        self.FSW.Set_AttnMech(10)
+        self.FSW.Set_AttnAuto()
         self.FSW.Get_IFOvld()
         self.FSW.Get_ACLR()
+        self.FSW.Set_DisplayUpdate('ON')
 
     def test_FSW_Freq(self):
         self.FSW.Set_FreqStart(10e6)
@@ -66,13 +73,13 @@ class TestGeneral(unittest.TestCase):
     def test_FSW_GetParams(self):
         nullVal = self.FSW.Get_Params_Amp(1)
         nullVal = self.FSW.Get_Params_Amp()
-        # nullVal = self.FSW.Get_Params_EVM()
         nullVal = self.FSW.Get_Params_Sweep(1)
         nullVal = self.FSW.Get_Params_Sweep()
         nullVal = self.FSW.Get_Params_Trace(1)
         nullVal = self.FSW.Get_Params_Trace()
         nullVal = self.FSW.Get_Params_System(1)
         nullVal = self.FSW.Get_Params_System()
+        nullVal = self.FSW.Get_Params(1,1,1,1,0)
 
     def test_FSW_GetScreenshot(self):
         getVal = self.FSW.Get_Screenshot()
@@ -80,11 +87,20 @@ class TestGeneral(unittest.TestCase):
     def test_FSW_GetTrace(self):
         getVal = self.FSW.Get_Trace_Data()
 
+    def test_FSW_Input(self):
+        self.FSW.Set_Freq(1e9)
+        self.FSW.Set_Input('RF')
+        self.FSW.Set_In_YIG('ON')
+        self.FSW.Set_In_HPFilter('ON')
+
     def test_FSW_Marker(self):
         self.FSW.Set_Mkr_Peak()
         getVal = self.FSW.Get_Mkr_Freq()
         getVal = self.FSW.Get_Mkr_XY()
         getVal = self.FSW.Get_Mkr_Y()
+
+    def test_FSW_Sys_Overload(self):
+        getVal = self.FSW.Get_Ovld_Stat()
 
 ###############################################################################
 ### </Test>
