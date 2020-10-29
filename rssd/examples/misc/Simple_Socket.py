18d5cdcc1a9ec640e6f00c2cc705cb837f9ca2ad
Martin Lim <trabin9@gmail.com>
2020-10-29 08:50:17 -0700

    chg formatting

--- rssd/examples/misc/Simple_Socket.py
+++ rssd/examples/misc/Simple_Socket.py
@@ -1,45 +1,41 @@
-##########################################################
-### Rohde & Schwarz Automation for demonstration use.
-###
-### Purpose: Simple Instrument Socket Example
-### Author:  mclim
-### Date:    2017.09.01
-##########################################################
-### User Entry
-##########################################################
-host = '192.168.1.109'           #Instrument IP address
-port = 5025                      #Instrument control port
+"""Rohde & Schwarz Automation for demonstration use. """
+#pylint: disable=invalid-name
 
-##########################################################
+import socket                                           #Import socket module
+import xml.etree.ElementTree as ET
+host = '192.168.1.109'                                  #Instrument IP address
+
+###############################################################################
 ### Code Begin
-##########################################################
-import socket                    #Import socket module
-import xml.etree.ElementTree as ET 
+###############################################################################
 
 def sQuery(SCPI):
-   out = SCPI + "\n"
-   s.sendall(out.encode())       #Write 'cmd'
-   sOut = s.recv(2048).strip()   #read socket
-   return sOut.decode()
+    '''socket query'''
+    out = SCPI + "\n"
+    s.sendall(out.encode())                             #Write 'cmd'
+    sOut = s.recv(2048).strip()                         #read socket
+    return sOut.decode()
 
 def sWrite(SCPI):
-   out = SCPI + "\n"
-   s.sendall(out.encode())       #Write 'cmd'
+    '''socket write'''
+    out = SCPI + "\n"
+    s.sendall(out.encode())                             #Write 'cmd'
 
 def getSysInfo():
-   xmlIn = sQuery("SYST:DFPR?")
-   strStart = xmlIn.find('deviceId="') + len('deviceID="')
-   strStop  = xmlIn.find('type="') - 2
-   xmlIn = xmlIn[strStart:strStop]#Remove header
-   print(xmlIn)
-   return xmlIn
-   
-##########################################################
+    '''get system options'''
+    xmlIn = sQuery("SYST:DFPR?")
+    strStart = xmlIn.find('deviceId="') + len('deviceID="')
+    strStop  = xmlIn.find('type="') - 2
+    xmlIn = xmlIn[strStart:strStop]                     #Remove header
+    print(xmlIn)
+    return xmlIn
+
+###############################################################################
 ### Main Code
-##########################################################
-s = socket.socket()              #Create a socket object
-s.connect((host, port))
-s.settimeout(1)                  #Timeout in seconds  
+###############################################################################
+s = socket.socket()                                     # Create a socket object
+s.connect((host, 5025))
+s.settimeout(1)                                         # Timeout in seconds
 
 print("Info:" + sQuery("*IDN?"))
 print("Opts:" + sQuery("*OPT?"))
