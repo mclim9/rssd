add57585c631873da1868b81385616a5a8d20b04
Martin Lim <trabin9@gmail.com>
2020-11-02 13:14:34 -0800

    3 --> 4 spaces

--- rssd/examples/misc/Simple_VISA.py
+++ rssd/examples/misc/Simple_VISA.py
@@ -1,36 +1,39 @@
-##########################################################
-### Rohde & Schwarz Automation for demonstration use.
-##########################################################
-host = '192.168.1.108'           #Instrument IP address
+'''Rohde & Schwarz Automation for demonstration use.'''
+#pylint: disable=invalid-name, unused-import, using-constant-test
+import xml.etree.ElementTree as ET
+import visa                             #Import VISA module
+
+host = '192.168.1.108'                  #Instrument IP address
 
 ##########################################################
 ### Code Begin
 ##########################################################
-import visa                      #Import VISA module
-import xml.etree.ElementTree as ET  
 
 def vQuery(SCPI):
-   vOut = VISA1.query(SCPI)      #Query cmd
-   return vOut.strip()
+    '''VISA query'''
+    vOut = VISA1.query(SCPI)            #Query cmd
+    return vOut.strip()
 
 def vWrite(SCPI):
-   VISA1.write(SCPI)             #Write cmd
+    '''VISA write'''
+    VISA1.write(SCPI)                   #Write cmd
 
 def getSysInfo():
-   xmlIn = vQuery("SYST:DFPR?")
+    '''Get System Info'''
+    xmlIn = vQuery("SYST:DFPR?")
 
-   xmlIn = xmlIn[xmlIn.find('>')+1:]#Remove header
-   root  = ET.fromstring(xmlIn)
-   if 0:
-      DData = root.find('DeviceData').items()
-      devID = DData[0][1]
-      dType = DData[1][1]
-   devID = root[0].attrib['deviceId']
-   dType = root[0].attrib['type']
-   # os    = root[2][1].attrib['name']
-   # osVer = root[2][1].attrib['version']
-   # print(os, osVer)
-   print(dType, devID)
+    xmlIn = xmlIn[xmlIn.find('>')+1:]   #Remove header
+    root  = ET.fromstring(xmlIn)
+    if 0:
+        DData = root.find('DeviceData').items()
+        devID = DData[0][1]
+        dType = DData[1][1]
+    devID = root[0].attrib['deviceId']
+    dType = root[0].attrib['type']
+    # os     = root[2][1].attrib['name']
+    # osVer = root[2][1].attrib['version']
+    # print(os, osVer)
+    print(dType, devID)
 
 ##########################################################
 ### Main Code
