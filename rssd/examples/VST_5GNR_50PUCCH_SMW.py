53c1ecaf4d1667652db1f640f30c72396cf04902
Martin Lim <trabin9@gmail.com>
2020-11-12 15:22:25 -0800

    Validate w/ SMW firmware.

--- rssd/examples/VST_5GNR_50PUCCH_SMW.py
+++ rssd/examples/VST_5GNR_50PUCCH_SMW.py
@@ -9,63 +9,55 @@ freqArry    = [28e9]
 ### Overhead
 ###############################################################################
 from rssd.VSG.NR5G_K144     import VSG
-# from rssd.VSA.NR5G_K144     import VSA
-# from rssd.FileIO            import FileIO
-# from rssd.RSI.time          import timer
 
-# OFile = FileIO().makeFile(__file__)
-# TMR = timer()
 SMW = VSG().jav_Open(SMW_IP)                          #Create SMW Object
 SMW.debug = 0
-# FSW = VSA().jav_Open(FSW_IP)                          #Create FSW Object
-# FSW.debug = 0
-
-# def NR5G_Rx_Init():
-#     """Start 5GNR Measurement Channel"""
-#     FSW.Init_5GNR()
-#     FSW.Set_5GNR_FrameCount('OFF')
 
 ###############################################################################
 ### Code Start
 ###############################################################################
-SMW.Set_Freq(28e9)
+SMW.Set_Freq(3.5e9)
 SMW.Set_5GNR_BBState('OFF')
 SMW.Set_5GNR_Direction('UL')
-SMW.Set_5GNR_BWP_CellID(777)
+# SMW.Set_5GNR_BWP_CellID(1024)
+SMW.write(':SOUR1:BB:NR5G:UBWP:USER0:UEID 1024')
 SMW.Set_5GNR_FreqRange('LOW')
 SMW.Set_5GNR_ChannelBW(100)
-SMW.Set_5GNR_BWP_Users(10)
+SMW.Set_5GNR_BWP_Users(50)
 
-for ch in range(0,10):
-    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:NALL 1')
-    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:CONT PUCC')
-    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:FMT F0')           # Format
-    # SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:RBN 1')          # Number of RB
-    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:RBOF {ch}')         # RB Offset
-    # SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:SYMN 2')         # Number of Symbol
-    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:SYM 0')            # Symbol offset
-    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:REP SLOT')         # Repitition
+for ch in range(0,44):
+    ## Frame Configuration
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:NALL 1')                   # Add User
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:CONT PUCC')           # Select PUCCH
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:FMT F0')              # Format
+    # SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:RBN 1')             # Number of RB
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:RBOF {ch}')           # RB Offset
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:SYMN 2')              # Number of Symbol
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:SYM 12')              # Symbol offset
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:REP SLOT')            # Repitition
+
+    ## PUCCH Setting
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:ISFH 1')         # Intra Slot Freq Hop
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:HOP {ch * 2}')   # Hop ID
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:SHOP {272-ch}')  # 2nd Hop PBR
     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:FS:CYCS 6')   # Initial Cyclic Shift
 
-# RBOffset = 50
-# RBSize = 16
-# for i in range(6):
-#     ch = i + 44
-#     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:NALL 1')
-#     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:CONT PUCC')
-#     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:FMT F2')           # Format
-#     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:RBN {RBSize}')     # Number of RB
-#     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:RBOF {RBOffset + i*RBSize}')         # RB Offset
-#     #MMM Causes Crash
-#     # SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:SYMN 2')         # Number of Symbol
-#     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:SYM 0')            # Symbol offset
-#     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:REP SLOT')         # Repitition
-#     SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:FS:CYCS 6')   # Initial Cyclic Shift
+RBSize = 1
+for ch in range(44,50):
+    ## Frame Configuration
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:NALL 1')                   # Add User
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:CONT PUCC')           # Select PUCCH
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:FMT F2')              # Format
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:RBN {RBSize}')        # Number of RB
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:RBOF {ch}')           # RB Offset
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:SYMN 2')              # Number of Symbol
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:SYM 0')               # Symbol offset
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:REP SLOT')            # Repitition
 
-
-# :SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER2:BWP0:ALL0:PUCCh:GRPHopping ENA
-# :SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER2:BWP0:ALL0:PUCCh:GRPHopping N
-# :SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER2:BWP0:ALL0:PUCCh:ISFHopping 1
-# :SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER2:BWP0:ALL0:PUCCh:SHOPping 8
+    ## PUCCH Setting
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:ISFH 1')         # Intra Slot Freq Hop
+    # SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:HOP {ch * 2}')   # Hop ID
+    SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:SHOP {272-ch}')  # 2nd Hop PBR
+    # SMW.write(f':SOUR1:BB:NR5G:SCH:CELL0:SUBF0:USER{ch}:BWP0:ALL0:PUCC:FS:CYCS 6')   # Initial Cyclic Shift
 
 SMW.Set_5GNR_BBState('ON')
