e3045f3bb6f967b532705cf235db5b7ba3d0a3c1
Martin Lim <trabin9@gmail.com>
2020-12-21 09:36:37 -0800

    new methods from Jim

--- rssd/DSO/Common.py
+++ rssd/DSO/Common.py
@@ -1,16 +1,13 @@
+"""Digital Storage Oscilloscope Common Functions"""
 # -*- coding: future_fstrings -*-
 ###############################################################################
-### Rohde & Schwarz Automation for demonstration use.
-### Purpose: Digital Storage Oscilloscope Common Functions
-### Author : Martin C Lim
-### Date   : 2019.05.28
-###  _____  _____   ____ _______ ____ _________     _______  ______
+###  _____  _____   ____ _______ ____ _________     _______  ______ 
 ### |  __ \|  __ \ / __ \__   __/ __ \__   __\ \   / /  __ \|  ____|
-### | |__) | |__) | |  | | | | | |  | | | |   \ \_/ /| |__) | |__
-### |  ___/|  _  /| |  | | | | | |  | | | |    \   / |  ___/|  __|
-### | |    | | \ \| |__| | | | | |__| | | |     | |  | |    | |____
+### | |__) | |__) | |  | | | | | |  | | | |   \ \_/ /| |__) | |__    
+### |  ___/|  _  /| |  | | | | | |  | | | |    \   / |  ___/|  __|  
+### | |    | | \ \| |__| | | | | |__| | | |     | |  | |    | |____ 
 ### |_|    |_|  \_\\____/  |_|  \____/  |_|     |_|  |_|    |______|
-###                         _            _           _
+###                         _            _           _ 
 ###                        | |          | |         | |
 ###             _   _ _ __ | |_ ___  ___| |_ ___  __| |
 ###            | | | | '_ \| __/ _ \/ __| __/ _ \/ _` |
@@ -26,16 +23,16 @@ class DSO(jaVisa):
         super(DSO, self).__init__()
         self.Model = "DSO"
 
-    #####################################################################
-    ### DSO Get Functions
-    #####################################################################
+#####################################################################
+### DSO Get Functions
+#####################################################################
     def Get_AcqTime(self):
         """ Seconds """
         rdStr = self.query(f':TIM:RANG?')
         return rdStr
 
-    def Get_ChState(self, Ch=1):
-        rdStr = self.query(f':CHAN{Ch}:STAT?')
+    def Get_chState(self, ch=1):
+        rdStr = self.query(f':chAN{ch}:STAT?')
         return rdStr
 
     def Get_SamplingRate(self):
@@ -53,48 +50,137 @@ class DSO(jaVisa):
         rdStr = self.query(f':TIM:SCAL?')
         return rdStr
 
-    def Get_Trace(self,Ch,Wave):
+    def Get_Trace(self,ch,Wave):
         """ ASCII Trace """
         self.write(f'FORM ASCII')
-        YVals = self.query(f':CHAN{Ch}:WAV{Wave}:DATA?')
-        Headr = self.queryFloatArry(f':CHAN{Ch}:WAV{Wave}:DATA:HEAD?')
-        # Start = Headr[0]
-        # Stops = Headr[1]
-        # Sampl = Headr[2]
-        return YVals, Headr
+        YVals = self.query(f':chAN{ch}:WAV{Wave}:DATA?')
+        Headr = self.queryFloatArry(f':chAN{ch}:WAV{Wave}:DATA:HEAD?')
+        Start = Headr[0]
+        Stops = Headr[1]
+        Sampl = Headr[2]
+        return YVals
 
-    #####################################################################
-    ### DSO Init Functions
-    #####################################################################
+###############################################################################
+###  DSO Get Cursor Functions
+###############################################################################
+
+    def Get_Cursor_X_Delta(self, ch=1):
+        rdStr = self.query(f':CURS{ch}:XDEL:VAL?')
+        return rdStr
+
+    def Get_Cursor_X_Delta_Inverse(self, ch=1):
+        rdStr = self.query(f':CURS{ch}:XDEL:INV?')
+        return rdStr
+
+    def Get_Cursor_Y_Delta(self, ch=1):
+        rdStr = self.query(f':CURS{ch}:YDEL:VAL?')
+        return rdStr
+
+    def Get_Cursor_Y_Delta_Slope(self, ch=1):
+        rdStr = self.query(f':CURS{ch}:YDEL:SLOP?')
+        return rdStr
+
+###############################################################################
+###  DSO Get Measurement Functions
+###############################################################################
+
+    def Get_Meas_Res_Actual(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:ACT? {mode}')
+        return rdStr
+
+    def Get_Meas_Res_Average(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:AVG? {mode}')
+        return rdStr
+
+    def Get_Meas_Res_Event_Count(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:EVTC? {mode}')
+        return rdStr
+
+    def Get_Meas_Res_Negetive_Peak(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:NPE? {mode}')
+        return rdStr
+
+    def Get_Meas_Res_Positive_Peak(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:PPE? {mode}')
+        return rdStr
+
+    def Get_Meas_Res_Reliability(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:REL? {mode}')
+        return rdStr
+
+    def Get_Meas_Res_RMS(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:RMS? {mode}')
+        return rdStr
+
+    def Get_Meas_Res_Wave_Count(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:WFMC? {mode}')
+        return rdStr
+
+    def Get_Meas_Res_Std_Dev(self, mode, ch):
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        rdStr = self.query(f':MEAS{ch}:RES:STDD? {mode}')
+        return rdStr  
+
+#####################################################################
+### DSO Init Functions
+#####################################################################
     def Init_Measurement(self):
         #Configure instrument measurment
         pass
 
-    #####################################################################
-    ### DSO Set Functions
-    #####################################################################
+
+#####################################################################
+### DSO Set Functions
+#####################################################################
     def Set_AcqTime(self,sec):
         """ Seconds """
         self.write(f':TIM:RANG {sec}')
 
-    def Set_ChCoupling(self,state, Ch=1):
-        """ AC DC DCL """
-        self.write(f':CHAN{Ch}:COUP {state}')           #Display Update State
+    def Set_Autoset(self):
+        self.write(f':AUT')
 
-    def Set_ChStatus(self,state, Ch=1):
+    def Set_chCoupling(self,state, ch):
+        """ AC DC DCL """
+        self.write(f':chAN{ch}:COUP {state}')        #Display Update State
+
+    def Set_chStatus(self,state, ch):
         """ ON OFF """
-        self.write(f':CHAN{Ch}:STAT {state}')           #Display Update State
+        self.write(f':chAN{ch}:STAT {state}')        #Display Update State
+
+    def Set_chState(self,state, ch):
+        """ ON OFF """
+        self.write(f':chAN{ch}:STAT {state}') 
 
     def Set_DisplayUpdate(self,state):
         """ ON OFF """
-        self.write(f'SYST:DISP:UPD {state}')            #Display Update State
+        self.write(f'SYST:DISP:UPD {state}')        #Display Update State
+
+    def Set_System_Preset(self):
+        self.write(f'SYST:PRES') 
 
     def Set_SweepCont(self, state):
         """ ON OFF """
         if ('ON' in state.upper()) or (state == 1):
-            self.write(f'RUN')                          #Continuous
+            self.write(f'RUN')                      #Continuous
         else:
-            self.write(f'SING')                         #Single
+            self.write(f'SING')                     #Single
 
     def Set_TimeRef(self,sec):
         """ Seconds """
@@ -108,17 +194,92 @@ class DSO(jaVisa):
         """ Seconds """
         self.write(f':ACQ:RES {sec}')
 
-    def Set_VoltOffset(self,volt, Ch=1):
+    def Set_VoltOffset(self,volt, ch):
         """ Volts """
-        self.write(f':CHAN{Ch}:OFFS {volt}')
+        self.write(f':chAN{ch}:OFFS {volt}')
 
-    def Set_VoltRange(self,volt, Ch=1):
+    def Set_VoltRange(self,volt, ch):
         """ Volts """
-        self.write(f':CHAN{Ch}:RANG {volt}')
+        self.write(f':chAN{ch}:RANG {volt}')
 
-    def Set_VoltScale(self,volt, Ch=1):
+    def Set_VoltScale(self,volt, ch):
         """ Volts """
-        self.write(f':CHAN{Ch}:SCAL {volt}')
+        self.write(f':chAN{ch}:SCAL {volt}')
+
+###############################################################################
+###  DSO Set Cursor Functions
+###############################################################################
+    def Set_Cursor_All_Off(self,ch):    
+        """  1 to 4 """
+        self.write(f':CURS{ch}:AOFF') 
+
+    def Set_Cursor_State(self,state,ch):   
+        """ ON OFF , 1 to 4 """
+        self.write(f':CURS{ch}:STAT {state}') 
+
+    def Set_Cursor_Source(self,source,ch):   
+        """ ON OFF , Mx CxWx x = 1 to 4 """
+        self.write(f':CURS{ch}:SOUR {source}') 
+
+    def Set_Cursor_Function(self,function,ch):   
+        """ HOR VERT PAIR ,  1 to 4 """
+        self.write(f':CURS{ch}:FUNC {function}')
+
+    def Set_Cursor_Tracking(self,state,ch):   
+        """ ON OFF ,  1 to 4 """
+        self.write(f':CURS{ch}:TRAC:STAT {state}')
+
+    def Set_Cursor_Style(self,style,ch):   
+        """ LINe LRHombus VLRHombus RHOMbus ,  1 to 4 """
+        self.write(f':CURS{ch}:STYL {style}')
+
+    def Set_Cursor_Max_Peak(self,ch):   
+        """  1 to 4 """
+        self.write(f':CURS{ch}:MAX:PEAK')
+
+###############################################################################
+###  DSO Set Measurement Functions
+###############################################################################
+
+    def Set_Meas_All_Off(self,ch):   
+        """  1 to 8 """
+        self.write(f':MEAS{ch}:AOFF')
+
+    def Set_Meas_All_On(self,ch):   
+        """  1 to 8 """
+        self.write(f':MEAS{ch}:AON')
+
+    def Set_Meas_Additional(self, mode, state, ch):   
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        self.write(f':MEAS{ch}:ADD {mode} {state}')
+
+    def Set_Meas_Clear_Statistics(self, ch):   
+        """ Clear Statistics """
+        self.write(f':MEAS{ch}:CLE')
+
+    def Set_Meas_Main(self, mode, ch):   
+        """ HIGH LOW AMP MAX MIN PDELta MEAN RMS STDD POVershoot 
+            AREA RTIMe FTIMe PPULse PER FREQ PULCnt DELay ..."""
+        self.write(f':MEAS{ch}:MAIN {mode}')
+
+    def Set_Meas_Enable(self, mode, ch):   
+        """ ON OFF, ch """
+        self.write(f':MEAS{ch}:ENAB {mode}')
+    
+    def Set_Meas_Source(self, mode, ch):   
+        """ CxWx, ch """
+        self.write(f':MEAS{ch}:ENAB {mode}')
+
+    def Set_Meas_Statistics(self,state,ch):   
+        """ ON OFF , ch """
+        self.write(f':MEAS{ch}:STAT:ENAB {state}')
+###############################################################################
+###  DSO Set Display Functions
+###############################################################################
+    def Set_Meas_Display_Position(self, mode):   
+        """ PREView FLOAt DOCK """
+        self.write(f':DISP:RES:DEF {mode}')
 
 ###############################################################################
 ### Debug Main.  Won't run when imported
@@ -127,3 +288,4 @@ if __name__ == "__main__":
     DSO_Inst = DSO().jav_Open("192.168.1.100")
     DSO_Inst.jav_IDN()
     DSO_Inst.jav_ClrErr()
+    
