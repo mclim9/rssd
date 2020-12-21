c45eaa56ccd1bf56d7dc4f36e65a47362d75e806
Martin Lim <trabin9@gmail.com>
2020-12-21 10:37:32 -0800

    add print statements

--- rssd/examples/misc/Simple_Socket_SMWy.py
+++ rssd/examples/misc/Simple_Socket_SMWy.py
@@ -12,13 +12,17 @@ port = 5025                         #Instrument control port
 ##########################################################
 def sQuery(SCPI):
     """Socket Query"""
+    print(f'Write: {SCPI}')
     out = SCPI + "\n"
     s.sendall(out.encode())         #Write 'cmd'
     sOut = s.recv(2048).strip()     #read socket
-    return sOut.decode()
+    sOut = sOut.decode()
+    print(f'Query: {sOut}')
+    return sOut
 
 def sWrite(SCPI):
     """Socket Write"""
+    print(f'Write: {SCPI}')
     out = SCPI + "\n"
     s.sendall(out.encode())         #Write 'cmd'
 
@@ -30,6 +34,6 @@ s.connect((host, port))
 s.settimeout(1)                     #Timeout in seconds
 
 sWrite(':SOURce1:BB:NR5G:SCHed:CELL0:SUBF0:USER0:BWPart0:ALLoc0:PUSCh:TXSCheme:CDMData 1')
-print(sQuery(':SOURce1:BB:NR5G:SCHed:CELL0:SUBF0:USER0:BWPart0:ALLoc0:PUSCh:TXSCheme:CDMData?'))
+sQuery(':SOURce1:BB:NR5G:SCHed:CELL0:SUBF0:USER0:BWPart0:ALLoc0:PUSCh:TXSCheme:CDMData?')
 sWrite(':SOURce1:BB:NR5G:SCHed:CELL0:SUBF0:USER0:BWPart0:ALLoc0:PUSCh:DMRS:POWer -10')
-print(sQuery(':SOURce1:BB:NR5G:SCHed:CELL0:SUBF0:USER0:BWPart0:ALLoc0:PUSCh:DMRS:POWer?'))
+sQuery(':SOURce1:BB:NR5G:SCHed:CELL0:SUBF0:USER0:BWPart0:ALLoc0:PUSCh:DMRS:POWer?')
