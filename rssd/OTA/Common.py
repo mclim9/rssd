1f34836e117ef90dfa8a2f5e8038bd47a6f0088d
Martin Lim <trabin9@gmail.com>
2021-01-05 09:36:59 -0800

    removed slash in Prototype Untested comment

--- rssd/OTA/Common.py
+++ rssd/OTA/Common.py
@@ -1,24 +1,24 @@
-"""OTA Common Functions
-###############################################################################
-###  _____  _____   ____ _______ ____ _________     _______  ______
-### |  __ \|  __ \ / __ \__   __/ __ \__   __\ \   / /  __ \|  ____|
-### | |__) | |__) | |  | | | | | |  | | | |   \ \_/ /| |__) | |__
-### |  ___/|  _  /| |  | | | | | |  | | | |    \   / |  ___/|  __|
-### | |    | | \ \| |__| | | | | |__| | | |     | |  | |    | |____
-### |_|    |_|  \_\\____/  |_|  \____/  |_|     |_|  |_|    |______|
-###                         _            _           _
-###                        | |          | |         | |
-###             _   _ _ __ | |_ ___  ___| |_ ___  __| |
-###            | | | | '_ \| __/ _ \/ __| __/ _ \/ _` |
-###            | |_| | | | | ||  __/\__ \ ||  __/ (_| |
-###             \__,_|_| |_|\__\___||___/\__\___|\__,_|
-###
-### Terms:  Azimuth:    Theta; Turntable;
-###         Elevation:  Phi; ATS1000 Arm;
-### OTA:    ATS1000     Great Circle Cut; Turntable & Elevation Arm
-###         ATS1500     Az over El;
-###         ATS1800     Az over El;
-###############################################################################"""
+"""
+OTA Common Functions
+ _____  _____   ____ _______ ____ _________     _______  ______
+|  __ ||  __ | / __ |__   __/ __ |__   __| |   / /  __ ||  ____|
+| |__) | |__) | |  | | | | | |  | | | |   | |_/ /| |__) | |__
+|  ___/|  _  /| |  | | | | | |  | | | |    |   / |  ___/|  __|
+| |    | | | || |__| | | | | |__| | | |     | |  | |    | |____
+|_|    |_|  |_||____/  |_|  |____/  |_|     |_|  |_|    |______|
+                        _            _           _
+                       | |          | |         | |
+            _   _ _ __ | |_ ___  ___| |_ ___  __| |
+           | | | | '_ || __/ _ |/ __| __/ _ |/ _` |
+           | |_| | | | | ||  __/|__ | ||  __/ (_| |
+            |__,_|_| |_||__|___||___/|__|___||__,_|
+
+Terms:  Azimuth:    Theta; Turntable;
+        Elevation:  Phi; ATS1000 Arm;
+OTA:    ATS1000     Great Circle Cut; Turntable & Elevation Arm
+        ATS1500     Az over El;
+        ATS1800     Az over El;
+"""
 from rssd.yaVISA     import jaVisa           #pylint: disable=E0611,E0401
 
 class OTA(jaVisa):
@@ -26,13 +26,13 @@ class OTA(jaVisa):
     def __init__(self):
         super(OTA, self).__init__()
         self.Model = "OTA"
-        self.EOL   = '\x00'
+        self.EOL   = '|x00'
         self.cmdWait = 0.05     #Seconds
 
     def query(self,cmd):                            #pragma: no cover
         cmd = cmd + self.EOL
-        self.K2.read_termination = '\x00'
-        self.K2.write_termination = '\x00'
+        self.K2.read_termination = '|x00'
+        self.K2.write_termination = '|x00'
         self.K2.sendall(cmd.encode())               #Write if connected
         sOut = self.K2.recv(2048).strip()           #read socket
         read = sOut.decode()
