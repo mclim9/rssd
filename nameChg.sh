09dac19f06c62ffb94312e0b870a765ba79a6c02
Martin Lim <trabin9@gmail.com>
2021-02-22 11:29:55 -0800

    chg names.

---
+++ nameChg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+WRONG_EMAIL="email@emailer.com"
+NEW_NAME="rsa23770"
+NEW_EMAIL="martin.lim@rsa.rohde-schwarz.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
