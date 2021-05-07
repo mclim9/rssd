8556d3437ef1f54a9d0ca38fab9685834fa9ed06
Martin Lim <trabin9@gmail.com>
2021-05-07 08:58:06 -0700

    constant changes

--- rssd/examples/AAA_VST.py
+++ rssd/examples/AAA_VST.py
@@ -6,23 +6,23 @@ from rssd.FileIO        import FileIO
 freqOffset = 60e6
 smwPwr      = -10
 OFile = FileIO().makeFile(__file__)                 # Create Output File
-SMW = VSG().jav_Open('169.254.58.114',OFile)        # Create Generator Object
-FSW = VSA().jav_Open('169.254.58.109',OFile)        # Create Analyzer Object
+SMW = VSG().jav_Open('192.168.58.114',OFile)        # Create Generator Object
+FSW = VSA().jav_Open('192.168.58.109',OFile)        # Create Analyzer Object
 
 OFile.write('SMWFreq,FreqOffset,SMWPwr,MkrBand,MkrCenter,MkrOffset,MkrDelta,SMWOff')
-Freqs = range(int(36e9), int(50e9), int(100e6))     # Frequency List
+Freqs = range(int(7e9), int(9e9), int(100e6))     # Frequency List
 SMW.Set_RFPwr(smwPwr)
-SMW.Set_IQMod(1)
+SMW.Set_IQMod(0)
 FSW.Set_Span(200e6)
 FSW.Set_SweepTime(0.500)
 FSW.Set_Trace_Detector('RMS')
 FSW.Set_Mkr_Band(100e6)
 for frq in Freqs:
+    ## SMW Power ON
     SMW.Set_RFState(1)
     SMW.Set_Freq(frq)
     FSW.Set_SweepCont(0)
     FSW.Set_Freq(frq)
-    ## SMW on Power
     FSW.Set_Autolevel()
     FSW.Set_InitImm()
     FSW.Set_Mkr_Freq(frq)
@@ -30,7 +30,8 @@ for frq in Freqs:
     MkrBandPr = FSW.Get_Mkr_Band()
     FSW.Set_Mkr_Freq(frq + freqOffset)
     MkrOffset = FSW.Get_Mkr_Y()
-    ## SMW off Power
+
+    ## SMW Power Off
     SMW.Set_RFState(0)
     FSW.Set_InitImm()
     MkrOff = FSW.Get_Mkr_Y()
--- rssd/examples/VSA_BWMarker.py
+++ rssd/examples/VSA_BWMarker.py
@@ -8,8 +8,8 @@ Span    = 110e6
 Avg     = 5
 
 ### Loops
-Repeat  = 1
-swpTimeArry = [2e-3, 3e-3, 5e-3, 1e-2, 2e-2, 3e-2, 5e-2, 1e-1, 2e-1, 3e-1, 5e-1]
+Repeat  = 10
+swpTimeArry = [0, 2e-3, 3e-3, 5e-3, 1e-2, 2e-2, 3e-2, 5e-2, 1e-1, 2e-1, 3e-1, 5e-1]
 rbwArry = [1e3, 3e3, 5e3, 1e4, 3e4, 5e4, 1e5, 3e5, 5e5, 1e6]
 
 ################################################################################
@@ -35,7 +35,6 @@ FSW.Set_Trace_Detector('RMS')
 FSW.Set_Span(Span)
 FSW.Set_Mkr_Freq(Freq)
 FSW.Set_Mkr_Band(100e6)
-FSW.Set_In_YIG('ON')
 FSW.Set_SweepCont(0)
 FSW.Set_Autolevel()
 # FSW.Set_Trig1_Source('Ext')
@@ -45,22 +44,24 @@ FSW.Set_Autolevel()
 ################################################################################
 #sDate = timeit.default_timer().strftime("%y%m%d-%H:%M:%S.%f") #Date String
 LoopParam   = 'Iter,RBW,SwpTime'
-OFile.write(f'{LoopParam},{FSW.Get_Params(1,1,1,1,1)},{FSW.Get_Params_MkrBand(1)},{TMR.Get_Params_Time(1)}')
+OFile.write(f'{LoopParam},{FSW.Get_Params(1,1,1,1,1)},{FSW.Get_Params_MkrBand(1)},{TMR.Get_Params_Time(1)},YIGStat,')
 
 for i in range(Repeat):
-    for SwpTime in swpTimeArry:
-        for rbw in rbwArry:
-            TMR.start()                                 # <thing we are timing>
-            FSW.Set_Channel('Spectrum')
-            FSW.Set_ResBW(rbw)
-            FSW.Set_SweepTime(0)
-            FSW.Set_InitImm()                           # Take Sweep
-            MeasData    = FSW.Get_Params_MkrBand()
-            TMR.tick()                                  # <\thing we are timing>
-            LoopParam   = f'{i},{rbw:8.0f},{SwpTime:5.3f}'
-            TotTime     = TMR.Get_Params_Time()
-            OutStr      = f'{LoopParam},{FSW.Get_Params(1,1,1,1,0)},{MeasData},{TotTime}'
-            OFile.write(OutStr)
+    for yig in ['ON','OFF']:
+        FSW.Set_In_YIG(yig)
+        for SwpTime in swpTimeArry:
+            for rbw in rbwArry:
+                TMR.start()                                 # <thing we are timing>
+                FSW.Set_Channel('Spectrum')
+                FSW.Set_ResBW(rbw)
+                FSW.Set_SweepTime(SwpTime)
+                FSW.Set_InitImm()                           # Take Sweep
+                MeasData    = FSW.Get_Params_MkrBand()
+                TMR.tick()                                  # <\thing we are timing>
+                LoopParam   = f'{i},{rbw:8.0f},{SwpTime:5.3f}'
+                TotTime     = TMR.Get_Params_Time()
+                OutStr      = f'{LoopParam},{FSW.Get_Params(1,1,1,1,0)},{MeasData},{TotTime},{yig}'
+                OFile.write(OutStr)
 
 ################################################################################
 ### Cleanup Automation
