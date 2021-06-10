00644fbcd32fa51bd1bc1273578758e063819815
Martin Lim <trabin9@gmail.com>
2021-06-10 13:01:45 -0700

    simplify comments

--- rssd/examples/AAA_CommandTime.py
+++ rssd/examples/AAA_CommandTime.py
@@ -1,21 +1,11 @@
-###############################################################################
-### Rohde & Schwarz Automation for demonstration use.
-### Title  : Timing SCPI Commands Example
-### Creatd : mclim, 2018.05.24
-###############################################################################
-### User Entry
-###############################################################################
-instru_ip  = '192.168.1.107'
-
-###############################################################################
-### Code Overhead: Import and create objects
-###############################################################################
+""" Timing SCPI Commands Example"""
 import  timeit
 from rssd.yaVISA_socket     import jaVisa
 from rssd.FileIO            import FileIO
 
 OFile = FileIO().makeFile(__file__)
-instr = jaVisa().jav_Open(instru_ip,OFile)                  #Create Object
+instr = jaVisa().jav_Open('192.168.58.109', OFile)                 #Create Object
+instr.K2.settimeout(30)
 
 ###############################################################################
 ### Code Start
@@ -23,21 +13,19 @@ instr = jaVisa().jav_Open(instru_ip,OFile)                  #Create Object
 OFile.write('Iter,CmdTime,Response')
 
 ALTime = []
-for i in range(10):
+for i in range(1):
     rdStr = ''
     tick = timeit.default_timer()
+
     ### <\thing we are timing>
     instr.query('INIT:IMM;*OPC?')
-    # instr.write(':INST:COUP:RLEV ON')
-    # rdStr = instr.query('FETC:SUMM:EVM:ALL?')
+    rdStr = instr.query('FETC:SUMM:EVM:ALL?')
     ### <\thing we are timing>
+
     ALTime.append(timeit.default_timer() - tick)
     OutStr = f'{i},{ALTime[i]:.6f},{rdStr}'
     OFile.write(OutStr)
 
 print(f'Avg time  : {sum(ALTime) / float(len(ALTime))} secs')
 
-###############################################################################
-### Cleanup Automation
-###############################################################################
 instr.jav_Close()
