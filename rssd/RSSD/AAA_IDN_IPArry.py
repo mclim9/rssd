e682885b006c9c9888893d847a8b57323a0698ca
mclim-rsa25545 <martin.lim@rsa.rohde-schwarz.com>
2021-08-31 15:51:37 -0700

    somehow missing.

--- rssd/RSSD/AAA_IDN_IPArry.py
+++
@@ -1,7 +0,0 @@
-"""Instrument IDN String"""
-from rssd.instrument import instr
-
-for IPAddr in ['192.168.58.109','192.168.58.114','127.0.0.1']:
-    kyber = instr().open(IPAddr)
-    print(kyber.query('*IDN?'))
-    kyber.close()
